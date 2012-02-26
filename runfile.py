5b140de8e5990eac27479d6fb9836be7d6d2293f
roll <roll@respect31.com>
2012-02-26 08:05:44 +0400

    minor fix

--- runfile.py
+++ runfile.py
@@ -4,9 +4,9 @@ from lib31.decorators.cachedproperty import cachedproperty
 
 class Runclass(ProgramRunclass):
     
+    def register(self):
+        pass
+    
     @cachedproperty
     def _package(self):
         return Package()
-    
-    def register(self):
-        pass
