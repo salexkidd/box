62da7f07eb254966cd1a712f9c26e076d0782caf
roll <roll@respect31.com>
2012-02-26 13:54:03 +0400

    minor change

--- runfile.py
+++ runfile.py
@@ -1,12 +1,9 @@
-from lib31.decorators.cachedproperty import cachedproperty
 from runclasses.package import PackageRunclass
 from package import Package
 
 class Runclass(PackageRunclass):
     
+    PACKAGE = Package
+    
     def register(self):
         pass
-    
-    @cachedproperty
-    def _package(self):
-        return Package()
