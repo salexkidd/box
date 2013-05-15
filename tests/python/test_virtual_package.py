a8b30d915be84dcf2a504e996120aeb4e7fad731
roll <roll@respect31.com>
2013-05-15 14:30:56 +0400

    added __file__ test

--- tests/python/test_virtual_package.py
+++ tests/python/test_virtual_package.py
@@ -1,21 +1,18 @@
 import os
-import sys
 import unittest
 from lib31.python import VirtualPackage
 
 class VirtualPackageTest(unittest.TestCase):  
     
-    def setUp(self):
-        self.package = VirtualPackage(self.get_path('package'))
-        
-    def get_path(self, *paths):
-        return os.path.join(os.path.dirname(__file__), *paths)
+    #Public
     
-    def test_object(self):
-        self.assertEqual(
-            self.package, 
-            sys.modules[self.package.__name__]
+    def setUp(self):
+        self._package = VirtualPackage(
+            os.path.join(os.path.dirname(__file__), 'package')
         )
 
     def test_file(self):
-        self.package.__file__
+        self.assertEqual(
+            self._package.__file__,
+            None,
+        )
