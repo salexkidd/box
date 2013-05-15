075a990a86ddd484a221cafc2282d8ff4056b4e9
roll <roll@respect31.com>
2013-05-15 12:03:33 +0400

    upd package

--- package.py
+++ package.py
@@ -4,28 +4,8 @@ class Package(Package):
 
     #Public
     
-    name = 'lib31'
-    install_requires = ['box>=0.3']
+    install_requires = ['box>=0.7']
     test_suite = 'nose.collector'
-    tests_require = ['nose']
-    
-    platforms=['Unix', 'POSIX']   
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: Unix',
-        'Operating System :: POSIX',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: System :: Systems Administration',
-        'Topic :: Utilities',               
-    ]
-    
+    tests_require = ['nose']    
     
 package = Package()
