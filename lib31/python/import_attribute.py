7eb13b9a1833d4291a0617715601d2bb445c166a
roll <roll@respect31.com>
2013-05-17 09:12:04 +0400

    mc

--- lib31/python/import_attribute.py
+++ lib31/python/import_attribute.py
@@ -2,9 +2,9 @@ from .import_module import import_module
 
 def import_attribute(name, path=None):
     try:
-        module_name, attribute_name = name.rsplit('.', 1)
+        module_name, attr_name = name.rsplit('.', 1)
     except ValueError:
         raise TypeError('Name requires to be in [.]module.attr form')
     module = import_module(module_name, path)
-    attribute = getattr(module, attribute_name)
-    return attribute
+    attr = getattr(module, attr_name)
+    return attr
