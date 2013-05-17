52620e99324774f00c4feb9cd52b51d2215ab244
roll <roll@respect31.com>
2013-05-17 09:35:21 +0400

    mf

--- lib31/python/import_attribute.py
+++ lib31/python/import_attribute.py
@@ -2,7 +2,7 @@ from .import_module import import_module
 
 def import_attribute(name, path=None):
     try:
-        module_name, attr_name = name.rsplit('.', 1)
+        module_name, attr_name = filter(None, name.rsplit('.', 1))
     except ValueError:
         raise TypeError('Name requires to be in [.]module.attr form')
     module = import_module(module_name, path)
---
+++ tests/python/test_import_attribute.py
@@ -0,0 +1,20 @@
+import os
+import unittest
+from lib31.python import import_attribute
+
+class ImportModuleTest(unittest.TestCase):
+    
+    def setUp(self):
+        self._path = os.path.join(os.path.dirname(__file__), 'folder')
+    
+    def test_import_attribute_absolute(self):
+        self.assertEqual(
+            import_attribute('unittest.TestCase'), 
+            unittest.TestCase,
+        )
+        
+    def test_import_attribute_relative(self):
+        self.assertEqual(
+            import_attribute('.module.attr', self._path), 
+            'attr',
+        )
