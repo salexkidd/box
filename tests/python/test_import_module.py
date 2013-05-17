d6422c583e188e3e91e524f85559fccc6cfe7137
roll <roll@respect31.com>
2013-05-17 08:56:56 +0400

    added no path test

--- tests/python/test_import_module.py
+++ tests/python/test_import_module.py
@@ -4,8 +4,11 @@ from lib31.python import import_module
 
 class ImportModuleTest(unittest.TestCase):
     
-    def test_import_module(self):
+    def test_import_module_absolute(self):
         self.assertEqual(
             import_module('unittest'), 
             unittest,
-        )             
+        )
+        
+    def test_import_module_relative_with_no_path(self):
+        self.assertRaises(TypeError, import_module, '.module')
