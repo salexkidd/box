5518fe66cb848321cfe0919b252fec465c3ad33b
roll <roll@respect31.com>
2013-09-23 08:14:13 +0400

    remove program-command dep

--- lib31/console/program.py
+++ lib31/console/program.py
@@ -1,25 +1,12 @@
 from abc import ABCMeta, abstractmethod
-from .command import Command
 
 class Program(metaclass=ABCMeta):
     
     #Public
         
     def __init__(self, argv):
-        self._argv = argv
+        self.argv = argv
         
     @abstractmethod
     def __call__(self):
-        pass #pragma: no cover
-            
-    #Protected
-    
-    #TODO: use cachedproperty
-    @property
-    def _command(self):
-        return Command(self._argv, schema=self._command_schema)
-    
-    @property
-    @abstractmethod    
-    def _command_schema(self):
-        pass #pragma: no cover                  
+        pass #pragma: no cover               
