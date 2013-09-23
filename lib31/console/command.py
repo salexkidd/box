59fcaa84446037bd399faea11ceb782e952a3de5
roll <roll@respect31.com>
2013-09-23 10:04:09 +0400

    mc

--- lib31/console/command.py
+++ lib31/console/command.py
@@ -12,7 +12,7 @@ class Command:
         },                      
         'arguments': {
             'parameters': {
-                'nargs':'?',
+                'nargs':'*',
                 'default': [],
             },             
         },
