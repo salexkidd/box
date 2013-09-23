02ebd4121e51461cb975b289d020bfb44dd68864
roll <roll@respect31.com>
2013-09-23 08:02:42 +0400

    improve command

--- lib31/console/command.py
+++ lib31/console/command.py
@@ -6,39 +6,28 @@ class Command:
     
     #Public
     
-    argv = []
-    schema = {
-        'config': {
-            'prog': 'name',
-            'add_help': False,
-        },                      
-        'arguments': {
-            'argument': {
-                'nargs':'?',
-            },
-        },
-        'options': {},
-    }
-    parser = ArgumentParser
-    
-    def __init__(self, argv, schema=None, parser=None):
+    def __init__(self, argv, schema):
         self.argv = argv        
-        self.schema = schema or self.schema
-        self.parser = parser or self.parser
-        self._parse()
+        self.schema = schema
         
     def __getattr__(self, name):
-        return getattr(self._parsed, name)
+        if not name.startswith('_'):
+            return getattr(self._parsed_args, name)
+        else:
+            raise AttributeError(name)
         
     #Protected
+    
+    _parser_class = ArgumentParser
 
-    def _parse(self):
-        self._parsed = self._parser.parse_args(self.argv[1:])
+    @cachedproperty
+    def _parsed_args(self):
+        return self._parser.parse_args(self.argv[1:])
         
     @cachedproperty
     def _parser(self):
         #TODO: fix config work
-        parser = self.parser(self.schema['config'])
+        parser = self._parser_class(self.schema['config'])
         for name, data in deepcopy(self.schema['arguments']).items():
             parser.add_argument(name, **data)
         for name, data in deepcopy(self.schema['options']).items():
