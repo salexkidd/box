0f45e4fac0a18d70f221b7efed7a6d75cd2539e3
roll <roll@respect31.com>
2013-10-04 08:03:38 +0400

    added managed_dict

--- lib31/python/__init__.py
+++ lib31/python/__init__.py
@@ -1,4 +1,5 @@
 from .cachedproperty import cachedproperty
 from .import_attribute import import_attribute
 from .import_module import import_module
+from .managed_dict import ManagedDict
 from .virtual_package import VirtualPackage
---
+++ lib31/python/managed_dict.py
@@ -0,0 +1,38 @@
+class ManagedDict(dict):
+    """
+    All mutations through __setitem__ and __delitem__
+    """
+    
+    #Public
+    
+    def clear(self):
+        for key in list(self.keys()):
+            del self[key]
+    
+    def pop(self, key, default=None):
+        value = self.get(key, default)
+        del self[key]
+        return value
+    
+    def popitem(self):
+        if self:
+            (key, value) = list(self.items())[-1]
+            del self[key]
+            return (key, value)
+        else:
+            return super(ManagedDict, self).popitem()
+    
+    def setdefault(self, key, default=None):
+        value = self.get(key, default)
+        self[key] = value
+        return value
+    
+    def update(self, other, **kwargs):
+        if hasattr(other, 'keys'):
+            for key in other:
+                self[key] = other[key]
+        else:
+            for key, value in other:
+                self[key] = value
+        for key in kwargs:
+            self[key] = kwargs[key]
