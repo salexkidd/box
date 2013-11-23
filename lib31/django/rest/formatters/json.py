f77af25e96327e1f2d0c6be4a455d65c8ea158e0
roll <roll@respect31.com>
2013-11-23 18:02:04 +0400

    mr

--- lib31/django/rest/formatters/json.py
+++ lib31/django/rest/formatters/json.py
@@ -2,7 +2,7 @@ json = __import__('json', level=0)
 from lib31.json import DateEncoder
 from ..formatter import RestFormatter
 
-class JSON(RestFormatter):
+class JSONFormatter(RestFormatter):
     
     #Public
     
