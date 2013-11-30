c899c579c7a8d6dd1e59857b64d92d645bb4dbfa
roll <roll@respect31.com>
2013-11-30 16:28:09 +0400

    added access to django global_settings in Profile

--- lib31/django/profile.py
+++ lib31/django/profile.py
@@ -1,8 +1,18 @@
 from abc import ABCMeta, abstractmethod
+from django.conf import global_settings
 
 class Profile(metaclass=ABCMeta):
     
     #Public
+    
+    def __getattr__(self, name):
+        try:
+            return getattr(global_settings, name)
+        except AttributeError:
+            profile_name = self.__class__.__name__
+            raise AttributeError(
+                'Profile "{0}" has no "{1}" attribute'.
+                format(profile_name, name))
     
     def export(self, scope):
         for name in dir(self):
