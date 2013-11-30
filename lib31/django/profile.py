b0ebd09dca0812313852db681acf2f97fdef4c73
roll <roll@respect31.com>
2013-11-30 16:56:27 +0400

    rm rm concrete settings from profile

--- lib31/django/profile.py
+++ lib31/django/profile.py
@@ -7,7 +7,7 @@ class Profile(metaclass=ABCMeta):
     
     def __getattr__(self, name):
         try:
-            return getattr(global_settings, name)
+            return getattr(global_settings, name.upper())
         except AttributeError:
             profile_name = self.__class__.__name__
             raise AttributeError(
@@ -18,57 +18,3 @@ class Profile(metaclass=ABCMeta):
         for name in dir(self):
             if not name.startswith('_'):
                 scope[name.upper()] = getattr(self, name)
-                
-    #Abstract settings
-    
-    @property
-    @abstractmethod
-    def root_urlconf(self):
-        pass #pragma: no cover
-
-    @property
-    @abstractmethod
-    def secret_key(self):
-        pass #pragma: no cover
-    
-    #Debug settings
-    
-    debug = False
-    
-    @property
-    def template_debug(self):
-        return self.debug
-    
-    #Internationalization
-    #https://docs.djangoproject.com/en/1.6/topics/i18n/
-    
-    language_code = 'en-us'  
-    time_zone = 'UTC'
-    use_l18n = True
-    use_l10n = True
-    use_tz = True
-    
-    #Application definition
-    
-    installed_apps = (
-        'django.contrib.admin',
-        'django.contrib.auth',
-        'django.contrib.contenttypes',
-        'django.contrib.sessions',
-        'django.contrib.messages',
-        'django.contrib.staticfiles',        
-    )
-    
-    middleware_classes = (
-        'django.contrib.sessions.middleware.SessionMiddleware',
-        'django.middleware.common.CommonMiddleware',
-        'django.middleware.csrf.CsrfViewMiddleware',
-        'django.contrib.auth.middleware.AuthenticationMiddleware',
-        'django.contrib.messages.middleware.MessageMiddleware',
-        'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    )
-    
-    #Databases
-    #https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-    
-    databases = {}
