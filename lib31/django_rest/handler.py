f9f941a3270fb3a0abfa828f48c240653aad920d
roll <roll@respect31.com>
2013-12-01 14:43:51 +0400

    impl handle

--- lib31/django_rest/handler.py
+++ lib31/django_rest/handler.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
 from lib31.django import Handler
-from .exceptions import BadRequest
 from .formatter import ProxyFormatter
 from .request import Request
 from .responder import ProxyResponder
@@ -13,15 +12,11 @@ class Handler(Handler):
         self._request = self._request_class(http_request, version, 
                                             format, resource, constraints)
                            
-    #TODO: reimpl!!!
     def handle(self):
-        respond = {}
-        try:
-            respond['data'] = self._responder.respond(self._parsed_constraints)
-        except BadRequest as exception:
-            respond['error'] = True
-            respond['message'] = exception.message
-        text = self._formatter.format(respond)
+        responder = self._responder_class(self._request)
+        response = responder.respond()
+        formatter = self._formatter_class(response)
+        text = formatter.format()
         return HttpResponse(text)
     
     #Protected
