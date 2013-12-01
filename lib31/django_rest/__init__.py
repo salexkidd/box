ead844743f60ac5adc20787418d724b792265883
roll <roll@respect31.com>
2013-12-01 13:15:09 +0400

    returned JSONFormatter

--- lib31/django_rest/__init__.py
+++ lib31/django_rest/__init__.py
@@ -1,7 +1,7 @@
 from .exceptions import (BadRequest, 
                          ConstraintsAreNotSuppported, FormatIsNotSuppported, 
                          ResourceIsNotSuppported, VersionIsNotSuppported)
-from .formatter import Formatter
+from .formatter import Formatter, JSONFormatter
 from .handler import Handler
 from .parser import Parser
 from .responder import Responder
--- lib31/django_rest/formatter.py
+++ lib31/django_rest/formatter.py
@@ -1,4 +1,6 @@
+import json
 from abc import ABCMeta, abstractmethod
+from lib31.json import DateEncoder
 
 class Formatter(metaclass=ABCMeta):
     
@@ -7,3 +9,11 @@ class Formatter(metaclass=ABCMeta):
     @abstractmethod
     def format(self):
         pass #pragma: no cover
+
+
+class JSONFormatter(Formatter):
+    
+    #Public
+    
+    def format(self, struct):
+        return json.dumps(struct, cls=DateEncoder)    
