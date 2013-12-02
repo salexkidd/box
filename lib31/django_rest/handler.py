c019fee15a403ca13778800ae1f8c4041a3c3f17
roll <roll@respect31.com>
2013-12-02 12:11:33 +0400

    added http404 to handler

--- lib31/django_rest/handler.py
+++ lib31/django_rest/handler.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from lib31.django import Handler
 from .presenter import MappingPresenter
 from .responder import MappingResponder
+from .exceptions import FormatIsNotSuppported, VersionIsNotSuppported
 
 class Handler(Handler):
     
@@ -9,10 +10,13 @@ class Handler(Handler):
     
     def handle(self, http_request, url_request):
         presenter = self._presenter_class()
-        responder = self._responder_class()        
-        request = presenter.parse(http_request, url_request) 
-        response = responder.respond(request)
-        text = presenter.build(response)
+        responder = self._responder_class()
+        try:
+            request = presenter.parse(http_request, url_request) 
+            response = responder.respond(request)
+            text = presenter.build(response)
+        except (VersionIsNotSuppported, FormatIsNotSuppported):
+            raise Http404()
         return HttpResponse(text)
     
     #Protected
