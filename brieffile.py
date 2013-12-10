819de0ea5cf4d9e19f61ee35bd440e444f75b0ad
roll <roll@respect31.com>
2013-12-10 17:57:36 +0400

    mg to sub

--- brieffile.py
+++
@@ -1,9 +0,0 @@
-from brief import FileInput, FileOutput
-from packgram.manage import PythonPackgramBrief
-
-class Brief(PythonPackgramBrief):
-
-    #Public
-    
-    input = FileInput('setup.tpl')
-    output = FileOutput('setup.py')
