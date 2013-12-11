6d68bbc7c3f4ff8e7c55755fc44e8f5dfde7749d
roll <roll@respect31.com>
2013-12-11 15:36:08 +0400

    added ordered_class

--- lib31/python/__init__.py
+++ lib31/python/__init__.py
@@ -2,4 +2,5 @@ from .cachedproperty import cachedproperty
 from .import_attribute import import_attribute
 from .import_module import import_module
 from .managed_dict import ManagedDict
+from .ordered_class import OrderedClassMeta
 from .virtual_package import VirtualPackage
---
+++ lib31/python/ordered_class.py
@@ -0,0 +1,11 @@
+from collections import OrderedDict
+
+class OrderedClassMeta(type):
+    
+    @classmethod
+    def __prepare__(mcl, name, bases):
+        return OrderedDict()
+    
+    def __new__(cls, name, bases, classdict):
+        classdict['__order__'] = tuple(classdict)
+        return type.__new__(cls, name, bases, classdict)
