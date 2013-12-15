13961599659301b602c4e173aa208397d8b614e2
roll <roll@respect31.com>
2013-12-15 08:50:03 +0400

    upd runfile

--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
 import os
 from setuptools import find_packages, setup
 
-package = {
+setup(
 
 	#Main
-
-    'name': 'lib31',
-	'version': '0.6.0',
-	'packages': find_packages(os.path.dirname(__file__) or '.', exclude=['tests*']),
-	'include_package_data': True,
-    'install_requires': ['packgram>=0.6'],
-    'tests_require': ['nose'],
-    'test_suite': 'nose.collector',
+	
+    name='lib31',
+	version='0.6.0',
+	packages=find_packages(os.path.dirname(__file__) or '.', exclude=['tests*']),
+	include_package_data=True,
+    install_requires=['packgram>=0.6'],     
+    tests_require=['nose'],
+    test_suite='nose.collector',
     
     #Description
     
-    'author': 'Respect31',
-    'author_email': 'post@respect31.com',
-    'maintainer': 'Respect31',
-    'maintainer_email': 'post@respect31.com',
-    'license': 'MIT License',    
-    'url': 'https://github.com/respect31/lib31',
-    'download_url': 'https://github.com/respect31/lib31/tarball/0.6.0',    
-    'classifiers': ['Development Status :: 3 - Alpha', 'Intended Audience :: Developers', 'License :: OSI Approved :: MIT License', 'Programming Language :: Python :: 3.3', 'Topic :: Software Development :: Libraries :: Python Modules'],    
-    'description': 'Lib31 is library to provide common functionality.',    
-    'long_description': '''Lib31
+    author='Respect31',
+    author_email='post@respect31.com',
+    maintainer='Respect31',
+    maintainer_email='post@respect31.com',
+    license='MIT License',    
+    url='https://github.com/respect31/lib31',
+    download_url='https://github.com/respect31/lib31/tarball/0.6.0',    
+    classifiers=['Development Status :: 3 - Alpha', 'Intended Audience :: Developers', 'License :: OSI Approved :: MIT License', 'Programming Language :: Python :: 3.3', 'Topic :: Software Development :: Libraries :: Python Modules'],    
+    description='Lib31 is library to provide common functionality.',    
+    long_description='''Lib31
 =====
 Lib31 is library to provide common functionality.
 
@@ -56,7 +56,4 @@ History
 - ported to Python 3.3 without previous version support
 - updated application programing interface''',
         
-}
-
-if __name__ == '__main__':
-    setup(**package)
+)
--- setup.tpl
+++ setup.tpl
@@ -1,32 +1,29 @@
 import os
 from setuptools import find_packages, setup
 
-package = {
+setup(
 
 	#Main
-
-    'name': '{{ name }}',
-	'version': '{{ version }}',
-	'packages': find_packages(os.path.dirname(__file__) or '.', exclude=['tests*']),
-	'include_package_data': True,
-    'install_requires': ['packgram>=0.6'],
-    'tests_require': ['nose'],
-    'test_suite': 'nose.collector',
+	
+    name='{{ name }}',
+	version='{{ version }}',
+	packages=find_packages(os.path.dirname(__file__) or '.', exclude=['tests*']),
+	include_package_data=True,
+    install_requires=['packgram>=0.6'],     
+    tests_require=['nose'],
+    test_suite='nose.collector',
     
     #Description
     
-    'author': '{{ author }}',
-    'author_email': '{{ author_email }}',
-    'maintainer': '{{ maintainer }}',
-    'maintainer_email': '{{ maintainer_email }}',
-    'license': '{{ license }}',    
-    'url': 'https://github.com/{{ author|lower }}/{{ name|lower }}',
-    'download_url': 'https://github.com/{{ author|lower }}/{{ name|lower }}/tarball/{{ version|lower }}',    
-    'classifiers': {{ classifiers }},    
-    'description': '{{ description }}',    
-    'long_description': '''{{ long_description }}''',
+    author='{{ author }}',
+    author_email='{{ author_email }}',
+    maintainer='{{ maintainer }}',
+    maintainer_email='{{ maintainer_email }}',
+    license='{{ license }}',    
+    url='https://github.com/{{ author|lower }}/{{ name|lower }}',
+    download_url='https://github.com/{{ author|lower }}/{{ name|lower }}/tarball/{{ version|lower }}',    
+    classifiers={{ classifiers }},    
+    description='{{ description }}',    
+    long_description='''{{ long_description }}''',
         
-}
-
-if __name__ == '__main__':
-    setup(**package)
+)
