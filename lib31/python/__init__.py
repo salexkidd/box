cfb38b1eaf878e82e7465e84604a0bed11778fa9
roll <roll@respect31.com>
2013-12-23 13:15:55 +0400

    rm remove_module

--- lib31/python/__init__.py
+++ lib31/python/__init__.py
@@ -1,6 +1,2 @@
 from .cachedproperty import cachedproperty
 from .managed_dict import ManagedDict
-from .remove_modules import remove_modules
-
-#Remove modules
-remove_modules(locals())
--- lib31/python/remove_modules.py
+++
@@ -1,9 +0,0 @@
-from types import ModuleType
-
-def remove_modules(scope, skip=[]):
-    """Remove all module objects from scope 
-       except ones with key in skip list"""
-    for key, attr in list(scope.items()):
-        if (isinstance(attr, ModuleType) and 
-            key not in skip):
-            del scope[key]
