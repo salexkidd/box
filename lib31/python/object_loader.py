da6aad6aa1d00513d9d2affdf306816f4e694bae
roll <roll@respect31.com>
2013-12-26 09:00:26 +0400

    changed ObjectLoader.list to generator

--- lib31/python/object_loader.py
+++ lib31/python/object_loader.py
@@ -7,40 +7,38 @@ class ObjectLoader:
     #Public
 
     def load(self, base_dir, file_pattern, recursively=False):
-        files = self._find_files(base_dir, file_pattern, recursively)
-        modules = self._import_modules(files)
-        objects = self._get_objects(modules)
-        return objects
+        for file in self._find_files(base_dir, file_pattern, recursively):
+            for module in self._import_modules(file):
+                for obj in self._get_objects(module):
+                    yield obj
             
     #Protected
     
     def _find_files(self, base_dir, file_pattern, recursively=False):
-        files = []
         for dirpath, _, filenames in os.walk(base_dir):
             for filename in filenames:
                 if re.match(file_pattern, filename):
                     file = os.path.join(dirpath, filename)
-                    files.append(file)
+                    yield file
             if not recursively and dirpath == base_dir:
                 break
-        return files
     
-    def _import_modules(self, files):
-        modules = []
-        for file in files:
+    def _import_modules(self, file):
+        try:
             loader = importlib.machinery.SourceFileLoader(file, file)
             module = loader.load_module(file)
-            modules.append(module)
-        return modules
+            yield module
+        except Exception:
+            pass
         
-    def _get_objects(self, modules):
-        objects = []
-        for module in modules:
-            for name in dir(module):
+    def _get_objects(self, module):
+        for name in dir(module):
+            try:
                 obj = getattr(module, name)
                 if self._filter_object(obj, module, name):
-                    objects.append(obj)
-        return objects
+                    yield obj
+            except Exception:
+                pass
     
     def _filter_object(self, obj, module, name):
         if name.startswith('_'):
