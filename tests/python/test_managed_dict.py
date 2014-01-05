1f13674b80a97aa71fb6688034481365e669a8f3
roll <roll@respect31.com>
2014-01-05 13:42:11 +0400

    added ManagedDict test stub

---
+++ tests/python/test_managed_dict.py
@@ -0,0 +1,10 @@
+import unittest
+from unittest.mock import Mock
+from lib31.python.managed_dict import ManagedDict
+
+class ManagedDictTest(unittest.TestCase):
+    
+    #Public
+    
+    def setUp(self):
+        pass
