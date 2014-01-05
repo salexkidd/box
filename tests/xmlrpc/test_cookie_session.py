44654e3f194d62c99e30d77b9f977c0f7691ab3e
roll <roll@respect31.com>
2014-01-05 14:30:05 +0400

    added CookieSession* test stubs

---
+++ tests/xmlrpc/test_cookie_session.py
@@ -0,0 +1,18 @@
+import unittest
+from lib31.xmlrpc.cookie_session import (CookieSessionServerProxy, 
+                                         CookieSessionTransport)
+
+class CookieSessionServerProxyTest(unittest.TestCase):
+
+    #Public
+
+    def test(self):
+        pass
+    
+    
+class CookieSessionTransportTest(unittest.TestCase):
+
+    #Public
+
+    def test(self):
+        pass    
