c7534c4c93ff8222e11bcabed4137c788cf032b9
roll <roll@respect31.com>
2014-01-06 12:55:32 +0400

    impl SettingsTest

--- tests/program/test_settings.py
+++ tests/program/test_settings.py
@@ -6,4 +6,16 @@ class SettingsTest(unittest.TestCase):
     #Public
 
     def setUp(self):
-        self.settings = Settings()
+        MockSettings = self._make_mock_settings_class()
+        self.settings = MockSettings()
+        
+    def test(self):
+        self.assertEqual(self.settings, {'name': 'value'})
+        
+    #Protected
+    
+    def _make_mock_settings_class(self):
+        class MockSettings(Settings):
+            #Public
+            name = 'value'
+        return MockSettings
