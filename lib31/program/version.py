7e628fcfbc74343f11a737db2275e7807f0a8237
roll <roll@respect31.com>
2014-01-06 15:24:20 +0400

    implemented VersionTest

--- lib31/program/version.py
+++ lib31/program/version.py
@@ -1,45 +1,31 @@
 class Version(str):
     
     #Public
-        
-    def __new__(cls):
-        substrings = []
-        for name in cls._elements:
-            try:
-                value = getattr(cls, name)
-            except AttributeError:
-                raise NotImplementedError(
-                    'You have to implement all of version '
-                    'elements: '+str(cls._elements)
-                )
-            substrings.append(cls._build_substring(name, value))
-        return str.__new__(cls, cls._build_string(substrings))       
+    
+    major = 0
+    minor = 0
+    micro = 0
+    level = 'final'
+    serial = 0
+      
+    def __new__(cls, version=None):
+        if not version:
+            version = str.__new__(cls)
+        return str.__new__(cls, version._as_string)
     
     @property
     def info(self):
-        """
-        Returns version tuple
-        """
-        return tuple(
-            getattr(self, name) for name in self._elements
-        )
-        
+        return self._as_tuple
+    
     #Protected
+              
+    @property
+    def _as_tuple(self):
+        return (self.major, self.minor, self.micro, self.level, self.serial)
     
-    _elements = [
-        'major',
-        'minor',
-        'micro',
-        'level',
-    ]
-    
-    @classmethod
-    def _build_substring(cls, name, value):
-        result = str(value)
-        if name == 'level' and value == 'final':
-            result = ''
-        return result
-
-    @classmethod
-    def _build_string(cls, substrings):
-        return '.'.join(item for item in substrings if item)
+    @property
+    def _as_string(self):
+        items = [self.major, self.minor, self.micro] 
+        if self.level != 'final':
+            items.append(self.level)
+        return '.'.join(map(str, items)) 
