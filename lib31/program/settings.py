b6b67debfa83ad83532de08c6e5d49666056d2db
roll <roll@respect31.com>
2014-01-06 15:24:29 +0400

    added updating to Settings

--- lib31/program/settings.py
+++ lib31/program/settings.py
@@ -3,8 +3,25 @@ class Settings(dict):
     #Public
     
     def __init__(self, *args, **kwargs):
+        self.clear()
+        self.update(self._as_dict)
+        
+    def __setattr__(self, name, value):
+        super().__setattr__(name, value)
+        type(self).__init__(self)
+        
+    def __delattr__(self, name):
+        super().__delattr__(name)
+        type(self).__init__(self)
+                    
+    #Protected        
+    
+    @property
+    def _as_dict(self):
+        items = {}
         for name in dir(self):
             if not name.startswith('_'):
                 value = getattr(self, name)
                 if not callable(value):
-                    self[name] = value
+                    items[name] = value
+        return items
