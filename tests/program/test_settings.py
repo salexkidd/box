aff7d1654d0c66c3463f22ab0d52658092d6b9b2
roll <roll@respect31.com>
2014-01-06 15:49:57 +0400

    added tests to SettingsTest

--- tests/program/test_settings.py
+++ tests/program/test_settings.py
@@ -10,12 +10,21 @@ class SettingsTest(unittest.TestCase):
         self.settings = MockSettings()
         
     def test(self):
-        self.assertEqual(self.settings, {'name': 'value'})
+        self.assertEqual(self.settings, {'name1': 'value1'})
+        
+    def test___setattr__(self):
+        self.settings.name2 = 'value2'
+        self.assertEqual(self.settings, {'name1': 'value1', 'name2': 'value2'})
+        
+    def test___delattr__(self):
+        self.settings.name2 = 'value2'
+        del self.settings.name2
+        self.assertEqual(self.settings, {'name1': 'value1'})        
         
     #Protected
     
     def _make_mock_settings_class(self):
         class MockSettings(Settings):
             #Public
-            name = 'value'
+            name1 = 'value1'
         return MockSettings
