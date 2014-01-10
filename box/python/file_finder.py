2be0979cf4ca34a448aa0a4f28e9634fe1b0a61e
roll <roll@respect31.com>
2014-01-10 16:22:36 +0400

    improve FileFinder

--- box/python/file_finder.py
+++ box/python/file_finder.py
@@ -1,41 +1,61 @@
 import os
 import re
 import fnmatch
+from abc import ABCMeta, abstractmethod
 
 class FileFinder:
 
     #Public
 
-    def find(self, filename, basedir='', max_depth=0, 
-             filters=[], iterators=[], reducers=[]):
-        files = self._find_files(
-            basedir, filename, max_depth, filters, iterators)
-        reduced_files = self._reduce_files(files, reducers)
+    def find(self, filename=None, basedir='.', max_depth=0, 
+             filters=[], iterators=[], processors=[], reducers=[]):
+        if filename:
+            filters = [FileFinderFilenameFilter(filename)]+filters
+        files = self._find(basedir, max_depth, filters, iterators, processors)
+        reduced_files = self._reduce(files, reducers)
         return reduced_files
             
     #Protected
     
     _walk_operator = staticmethod(os.walk)
     
-    def _find_files(self, basedir, pattern, max_depth, filters, iterators):
+    def _find(self, basedir, max_depth, filters, iterators, processors):
         for dirpath, _, filenames in self._walk_operator(basedir):
-            if self._calculate_depth(basedir, dirpath) > max_depth:
+            depth = self._calculate_depth(basedir, dirpath)
+            if  depth > max_depth:
                 break            
             for filename in filenames:
-                filepath = os.path.join(dirpath, filename)
-                if not self._filter_by_filename(filename, pattern):
+                file = os.path.join(dirpath, filename)
+                if not self._filter(file, filters):
                     continue
-                if not self._filter_by_filepath(filepath, filters):
+                if not self._iterate(file, iterators):
                     continue
-                if not self._iterate_by_filepath(filepath, iterators):
-                    continue
-                yield filepath
+                processed_file = self._process(file, processors)
+                yield processed_file
     
-    def _reduce_files(self, files, reducers):
+    def _filter(self, file, filters):
+        for fltr in filters:
+            if not fltr(file):
+                return False
+        return True
+    
+    def _iterate(self, file, iterators):
+        for iterator in iterators:
+            if not iterator(file):
+                return False
+        return True    
+    
+    def _process(self, file, processors):
+        processed_file = file
+        for processor in processors:
+            processed_file = processor(processed_file)
+        return processed_file
+    
+    def _reduce(self, files, reducer):
         reduced_files = files
-        for reducer in reducers:
+        for reducer in reducer:
             reduced_files = reducer(reduced_files)
-        return reduced_files
+        return reduced_files   
     
     def _calculate_depth(self, basedir, dirpath):
         basedir = os.path.normpath(basedir)
@@ -47,28 +67,34 @@ class FileFinder:
         else:
             subpath = dirpath.replace(basedir+os.path.sep, '', 1)
             depth = subpath.count(os.path.sep)+1
-        return depth
+        return depth    
     
-    def _filter_by_filename(self, filename, pattern):
-        if self._is_object_regexp_compiled_pattern(pattern):
-            if not re.match(pattern, filename):
+    
+class FileFinderFilter(metaclass=ABCMeta):
+    
+    #Public
+           
+    @abstractmethod
+    def __call__(self, file, *args, **kwargs):
+        pass #pragma: no cover
+    
+    
+class FileFinderFilenameFilter(FileFinderFilter):
+    
+    #Public
+    
+    def __init__(self, filename):
+        self._filename = filename
+        
+    def __call__(self, file, *args, **kwargs):
+        filename = os.path.basename(file)
+        if self._is_object_regexp_compiled_pattern(self._filename):
+            if not re.match(self._filename, filename):
                 return False
         else:
-            if not fnmatch.fnmatch(filename, pattern):
+            if not fnmatch.fnmatch(filename, self._filename):
                 return False
         return True
-    
-    def _filter_by_filepath(self, filepath, filters):
-        for fltr in filters:
-            if not fltr(filepath):
-                return False
-        return True
-    
-    def _iterate_by_filepath(self, filepath, iterators):
-        for iterator in iterators:
-            if not iterator(filepath):
-                return False
-        return True    
     
     def _is_object_regexp_compiled_pattern(self, obj):
-        return isinstance(obj, type(re.compile('')))
+        return isinstance(obj, type(re.compile('')))    
--- tests/component/python/test_file_finder.py
+++ tests/component/python/test_file_finder.py
@@ -9,44 +9,49 @@ class FileFinderTest(unittest.TestCase):
     
     def setUp(self):
         walk_items = [
-            ['', [], ['filename1', 'filename2']],
-            ['folder', [], ['filename1', 'filename2']],
-            ['folder/subfolder', [], ['filename3']],
+            ['', [], ['file1', 'file2']],
+            ['folder', [], ['file1', 'file2']],
+            ['folder/subfolder', [], ['file3']],
         ]
         MockFileFinder = self._make_mock_file_finder_class(walk_items)
         self.finder = MockFileFinder()
 
     def test_find(self):
-        files = list(self.finder.find('filename1'))
-        self.assertEqual(files, ['filename1'])
+        files = list(self.finder.find('file1'))
+        self.assertEqual(files, ['file1'])
         
     def test_find_with_max_depth_is_1(self):
-        files = list(self.finder.find('filename1', max_depth=1))
-        self.assertEqual(files, ['filename1', 'folder/filename1'])
+        files = list(self.finder.find('file1', max_depth=1))
+        self.assertEqual(files, ['file1', 'folder/file1'])
         
     def test_find_with_max_depth_is_2(self):
-        files = list(self.finder.find('filename3', max_depth=2))
-        self.assertEqual(files, ['folder/subfolder/filename3'])        
+        files = list(self.finder.find('file3', max_depth=2))
+        self.assertEqual(files, ['folder/subfolder/file3'])        
         
     def test_find_with_regexp(self):
-        regex = re.compile('filename1+')
-        files = list(self.finder.find(regex))
-        self.assertEqual(files, ['filename1'])     
+        regexp = re.compile('file1+')
+        files = list(self.finder.find(regexp))
+        self.assertEqual(files, ['file1'])     
     
     def test_find_with_filters(self):
         filters = [lambda file: False]
-        files = list(self.finder.find('filename1', filters=filters))
+        files = list(self.finder.find('file1', filters=filters))
         self.assertEqual(files, [])
         
     def test_find_with_iterators(self):
         iterators = [lambda file: False]
-        files = list(self.finder.find('filename1', iterators=iterators))
-        self.assertEqual(files, [])        
+        files = list(self.finder.find('file1', iterators=iterators))
+        self.assertEqual(files, [])
+        
+    def test_find_with_processors(self):
+        processors = [lambda file: 'processed_'+file]
+        files = list(self.finder.find('file1', processors=processors))
+        self.assertEqual(files, [ 'processed_file1'])             
           
     def test_find_with_reducers(self):
         reducers=[lambda files: list(files)[0]]
-        files = self.finder.find('filename1', reducers=reducers)
-        self.assertEqual(files, 'filename1')               
+        files = self.finder.find('file1', reducers=reducers)
+        self.assertEqual(files, 'file1')               
     
     #Protected
     
