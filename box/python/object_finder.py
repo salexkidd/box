ece4ca0a66ede75eedede81ed0d804df7a68e457
roll <roll@respect31.com>
2014-01-11 10:15:09 +0400

    added ObjectFinder stub

---
+++ box/python/object_finder.py
@@ -0,0 +1,8 @@
+class ObjectFinder:
+    
+    #Public
+    
+    #TODO: add ignore_errors flag
+    def find(self, attrname=None, filename=None, basedir='.', max_depth=0, 
+             breakers=[], filters=[], processors=[], reducers=[]):
+        pass                
