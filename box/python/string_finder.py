2412dc282c9210dd4bc16bfaec721554f3ad960f
roll <roll@respect31.com>
2014-01-11 10:29:24 +0400

    reimplemented StringFinder

--- box/python/string_finder.py
+++ box/python/string_finder.py
@@ -9,52 +9,29 @@ class StringFinder:
     #TODO: add ignore_errors flag
     def find(self, pattern, filename=None, basedir='.', max_depth=0, 
              breakers=[], filters=[], processors=[], reducers=[]):
-        file_finder = self._file_finder_class()
-        file_finder_reducer = self._file_finder_reducer_class(
-            pattern, breakers, filters, processors, reducers)
-        map_reduced_strings = file_finder.find(
-            filename, basedir, max_depth, reducers=[file_finder_reducer])
+        strings = self._get_strings(pattern, filename, basedir, max_depth)
+        map_reduce = MapReduce(breakers, filters, processors, reducers)
+        map_reduced_strings = map_reduce(strings)
         return map_reduced_strings
     
     #Protected
     
-    _file_finder_class = FileFinder
-    _file_finder_reducer_class = property(
-        lambda self: StringFinderFileFinderReducer)
-    
-    
-class StringFinderFileFinderReducer:
-    
-    #Public
-    
-    def __init__(self, pattern, 
-                 breakers=[], filters=[], processors=[], reducers=[]):
-        self._pattern = pattern
-        self._breakers = breakers
-        self._filters = filters
-        self._processors = processors
-        self._reducers = reducers
-        
-    def __call__(self, files):
-        strings = self._get_strings(files)
-        map_reduce = MapReduce(
-            self._breakers, self._filters, self._processors, self._reducers)
-        map_reduced_strins = map_reduce(strings)
-        return map_reduced_strins
-    
-    #Protected
-    
     _open_operator = staticmethod(open)
+    _file_finder_class = FileFinder    
     
-    def _get_strings(self, files):
-        for file in files:
+    def _get_strings(self, pattern, filename, basedir, max_depth):
+        for file in self._get_files(filename, basedir, max_depth):
             with self._open_operator(file) as file_object:
-                pattern = self._pattern
                 if not self._is_object_regexp_compiled_pattern(pattern):
                     pattern = re.compile(pattern)
                 strings = pattern.findall(file_object.read())
                 for string in strings:
                     yield (string, file)
+                    
+    def _get_files(self, filename, basedir, max_depth):
+        file_finder = self._file_finder_class()
+        files = file_finder.find(filename, basedir, max_depth)
+        return files
     
     def _is_object_regexp_compiled_pattern(self, obj):
         return isinstance(obj, type(re.compile('')))                  
--- tests/component/python/test_string_finder.py
+++ tests/component/python/test_string_finder.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import Mock, mock_open, call
-from box.python.string_finder import StringFinder, StringFinderFileFinderReducer
+from box.python.string_finder import StringFinder
 
 class StringFinderTest(unittest.TestCase):
     
@@ -12,58 +12,23 @@ class StringFinderTest(unittest.TestCase):
         self.finder = MockStringFinder()
         
     def test_find(self):
-        strings = self.finder.find('pattern', 
-            filename='filename', 
-            basedir='basedir', 
-            max_depth='max_depth', 
-            breakers='breakers', 
-            filters='filters', 
-            processors='processors', 
-            reducers='reducers')
-        self.assertEqual(strings, ['strings'])
-        self.finder._file_finder_class.assert_called_with()
-        self.finder._file_finder_reducer_class.assert_called_with(
-            'pattern', 'breakers', 'filters', 'processors', 'reducers')
-        self.finder._file_finder_class.return_value.find.assert_called_with(
-            'filename', 'basedir', 'max_depth', reducers=['reducer'])
+        strings = list(self.finder.find('(da|ta)'))
+        self.assertEqual(strings, ['da', 'ta', 'da', 'ta'])
+        self.finder._open_operator.assert_has_calls([
+            call('file1'), call('file2')], any_order=True)
+        
+    def test_find_with_processor(self):
+        processor = lambda string, file: file+':'+string
+        strings = list(self.finder.find('(da|ta)', processors=[processor]))
+        self.assertEqual(
+            strings, ['file1:da', 'file1:ta', 'file2:da', 'file2:ta'])       
     
     #Protected
 
     def _make_mock_string_finder_class(self, files):
         class MockStringFinder(StringFinder):
             #Protected
-            _file_finder_class = Mock(return_value=Mock(
-                find=Mock(return_value=['strings'])))
-            _file_finder_reducer_class = Mock(return_value='reducer')
-        return MockStringFinder
-    
-
-class StringFinderFileFinderReducerTest(unittest.TestCase):
-
-    #Public
-
-    def setUp(self):
-        self.Reducer = self._make_mock_reducer_class()
-        self.files = ['file1', 'file2']
-        
-    def test___call__(self):
-        reducer = self.Reducer('(da|ta)')
-        strings = list(reducer(self.files))
-        self.assertEqual(strings, ['da', 'ta', 'da', 'ta'])
-        reducer._open_operator.assert_has_calls([
-            call('file1'), call('file2')], any_order=True)
-        
-    def test___call___with_processor(self):
-        processor = lambda string, file: file+':'+string
-        reducer = self.Reducer('(da|ta)', processors=[processor])
-        strings = list(reducer(self.files))
-        self.assertEqual(
-            strings, ['file1:da', 'file1:ta', 'file2:da', 'file2:ta'])       
-    
-    #Protected
-    
-    def _make_mock_reducer_class(self):
-        class MockReducer(StringFinderFileFinderReducer):
-            #Protected
             _open_operator = mock_open(read_data='data')
-        return MockReducer
+            _file_finder_class = Mock(return_value=Mock(
+                find=Mock(return_value=files)))
+        return MockStringFinder
