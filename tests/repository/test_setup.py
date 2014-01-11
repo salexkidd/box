67da0f8700fb5f05f26c6d4c1746fd2801c39a34
roll <roll@respect31.com>
2014-01-11 14:40:31 +0400

    rebased SetupTest on ObjectFinder

--- tests/repository/test_setup.py
+++ tests/repository/test_setup.py
@@ -1,32 +1,24 @@
 import os
 import unittest
 from box import version
-from box.python import ObjectLoader
+from box.python import ObjectFinder
 
 class SetupTest(unittest.TestCase):
 
     #Public
     
     def setUp(self):
-        PackageObjectLoader = self._make_object_loader_class()
-        self.basedir = os.path.join(os.path.dirname(__file__), '..', '..')
-        self.loader = PackageObjectLoader()
-        self.objects = list(self.loader.load(self.basedir, 'setup.py'))
+        self.basedir = self._get_repository_path()
+        self.finder = ObjectFinder()
 
     def test(self):
-        package = self.objects[0]
+        package = self.finder.find(
+            'package', 'setup.py', self.basedir,
+            reducers=[lambda objects: list(objects)[0]])
         self.assertEqual(package['name'], 'box')
         self.assertEqual(package['version'], version)
         
     #Protected
     
-    def _make_object_loader_class(self):
-        class PackageObjectLoader(ObjectLoader):
-            #Protected
-            def _filter_object(self, obj, module, name):
-                if not super()._filter_object(obj, module, name):
-                    return False
-                if name != 'package':
-                    return False
-                return True
-        return PackageObjectLoader
+    def _get_repository_path(self, *args):
+        return os.path.join(os.path.dirname(__file__), '..', '..')
