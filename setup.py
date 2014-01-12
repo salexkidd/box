3f4d1c5bd2e1f5e946897ce2291b01b1f32638ba
roll <roll@respect31.com>
2014-01-12 17:54:25 +0400

    added documentation badge

--- setup.py
+++ setup.py
@@ -41,6 +41,9 @@ Box is library to provide common functionality.
 .. image:: https://coveralls.io/repos/respect31/box/badge.png?branch=master 
      :target: https://coveralls.io/r/respect31/box  
      :alt: coverage
+.. image:: http://b.repl.ca/v1/docs-latest-brightgreen.png
+     :target: http://box.readthedocs.org/en/latest/
+     :alt: docs
      
 Quick Links
 -----------
