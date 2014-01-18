15f100691c03f9e97c385d978a9ee9d03af21f77
roll <roll@respect31.com>
2014-01-18 14:01:00 +0400

    refactored render_file

--- box/jinja2/render_file.py
+++ box/jinja2/render_file.py
@@ -6,42 +6,68 @@ class RenderFile:
     
     #Public
     
-    def __call__(self, context, source, target=None):
-        template = self._get_template(source)
+    def __call__(self, path, context={}, target=None):
+        dirpath = self._get_dirname(path)
+        filename = self._get_filename(path)
+        loader = self._get_loader(dirpath)
+        environment = self._get_environment(loader, context)
+        template = self._get_template(environment, filename)
         context = self._get_context(context)
         text = template.render(context)
         if target:
-            with self._open_function(target, 'w') as file:
-                file.write(text)
-        else:
-            return text
+            self._write_text(text, target)
+        return text
             
     #Protected
     
     _object_context_class = ObjectContext
     _open_function = staticmethod(open)
     
-    def _get_template(self, source):
-        dirname, filename = os.path.split(os.path.abspath(source))
-        loader = self._file_system_loader_class(dirname)
+    def _get_dirname(self, path):
+        return os.path.dirname(path)
+    
+    def _get_filename(self, path):
+        return os.path.basenmae(path)
+    
+    def _get_loader(self, dirpath):
+        return self._file_system_loader_class(dirpath)
+    
+    def _get_environment(self, loader, context):
         environment = self._environment_class(loader=loader)
-        environment.template_class = self._module_template_class
-        template = environment.get_template(filename)
-        return template
+        if not self._is_object_jinja2_context(context):        
+            environment.template_class = self._object_template_class
+        return environment
+    
+    def _get_template(self, environment, filename):
+        return environment.get_template(filename)
     
     def _get_context(self, context):
-        context = self._module_context_class(self.meta_module)
+        if not self._is_object_jinja2_context(context):
+            context = self._object_context_class(context)
         return context
     
-    def _get_environment_class(self):
+    def _write_text(self, text, target):
+        with self._open_function(target, 'w') as file:
+            file.write(text)        
+    
+    def _is_object_jinja2_context(self, obj):
+        if hasattr(obj, '__contains__') and hasattr(obj, '__getitem__'):
+            return True
+        else:
+            return False
+    
+    @property
+    def _environment_class(self):
         from jinja2 import Environment
         return Environment
     
-    def _get_file_system_loader_class(self):
+    @property
+    def _file_system_loader_class(self):
         from jinja2 import FileSystemLoader
         return FileSystemLoader    
     
-    def _get_object_template_class(self):
+    @property
+    def _object_template_class(self):
         from jinja2 import Template
         class ObjectTemplate(ObjectTemplateMixin, Template): pass
         return ObjectTemplate
--- tests/component/jinja2/test_render_file.py
+++ tests/component/jinja2/test_render_file.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import Mock, mock_open
-from box.jinja2.render_file import render_file, ModuleTemplate, ModuleContext
+from box.jinja2.render_file import RenderFile
 
 class RenderFileTaskTest(unittest.TestCase):
 
@@ -8,41 +8,46 @@ class RenderFileTaskTest(unittest.TestCase):
 
     def setUp(self):
         self.template = Mock(render=Mock(return_value='text'))
-        MockRenderFileTask = self._make_mock_render_file_task_class(self.template)
+        self.render_file = self._make_mock_render_file_function(self.template)
         self.source = '/source'
         self.target = '/target'
-        self.task = MockRenderFileTask(module=None)
         
     def test_complete(self):
-        self.task.complete(self.source, self.target)
-        self.task._open_operator.assert_called_with(self.target, 'w')
-        self.task._open_operator().write.assert_called_with('text')
+        self.render_file(self.source, self.target)
+        self.render_file._open_function.assert_called_with(self.target, 'w')
+        self.render_file._open_function().write.assert_called_with('text')
         
     def test__get_template(self):
-        self.assertEqual(self.task._get_template(self.source), self.template)
-        self.task._file_system_loader_class.assert_called_with('/')
-        self.task._environment_class.assert_called_with(loader='loader')
+        self.assertEqual(self.render_file._get_template(self.source), self.template)
+        self.render_file._file_system_loader_class.assert_called_with('/')
+        self.render_file._environment_class.assert_called_with(loader='loader')
         self.assertEqual(
-            self.task._environment_class.return_value.template_class,
-            self.task._module_template_class)
-        (self.task._environment_class.return_value.get_template.
+            self.render_file._environment_class.return_value.template_class,
+            self.render_file._module_template_class)
+        (self.render_file._environment_class.return_value.get_template.
             assert_called_with('source'))
         
-    def test__get_context(self):
-        self.assertEqual(self.task._get_context(), 'context')
-        self.task._module_context_class.assert_called_with('module')        
+    def test__get_context_from_dict(self):
+        context = dict()
+        self.assertEqual(self.render_file._get_context(context), context)
+        
+    def test__get_context_from_object(self):
+        context = object()
+        self.assertEqual(self.render_file._get_context(context), 'context')
+        self.render_file._object_context_class.assert_called_with(context)   
     
     #Protected
     
-    def _make_mock_render_file_task_class(self, template):
-        class MockRenderFileTask(RenderFileTask):
+    def _make_mock_render_file_function(self, template):
+        class MockRenderFile(RenderFile):
             #Public
             meta_module = 'module'
             #Protected
+            _object_context_class = Mock(return_value='context')
+            _open_function = mock_open()
             _environment_class = Mock(return_value=Mock(
                 get_template=Mock(return_value=template)))
             _file_system_loader_class = Mock(return_value='loader')
-            _module_template_class = Mock()
-            _module_context_class = Mock(return_value='context')
-            _open_operator = mock_open()
-        return MockRenderFileTask
+            _object_template_class = Mock()
+        mock_render_file = MockRenderFile()
+        return mock_render_file
