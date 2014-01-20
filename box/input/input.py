c175f3ce43c9b7996a0120d3b33b6cfb84b5d161
roll <roll@respect31.com>
2014-01-20 09:52:51 +0400

    added default_error

--- box/input/input.py
+++ box/input/input.py
@@ -18,12 +18,15 @@ class InputCall:
     
     def __init__(self, prompt, error=None, 
                  default=None, options=None, attempts=None,
+                 prompt_template=None, error_template=None,
                  input_function=None, print_function=None):
         self._prompt = prompt
         self._error = error or self._default_error
         self._default = default
         self._options = options
         self._attempts = attempts or self._default_attempts
+        self._prompt_template = prompt_template or self._default_prompt_template
+        self._error_template = error_template or self._default_error_template
         self._input_function = input_function or self._default_input_function
         self._print_function = print_function or self._default_print_function
         
@@ -46,26 +49,30 @@ class InputCall:
     
     #Protected
     
-    _default_error = 'Try again..'    
-    _default_attempts = 3    
+    _default_error = 'Try again..'
+    _default_attempts = 3
+    _default_prompt_template = '{{ prompt }}'
+    _default_error_template = '{{ error }}'       
     _default_input_function = staticmethod(input)
     _default_print_function = staticmethod(print)
     
     @property
     def _rendered_prompt(self):
-        template = self._template_class(self._prompt)
+        template = self._template_class(self._prompt_template)
         rendered_prompt = template.render(self._context)
         return rendered_prompt
 
     @property
     def _rendered_error(self):
-        template = self._template_class(self._error)
+        template = self._template_class(self._error_template)
         rendered_error = template.render(self._context)
         return rendered_error
     
     @property
     def _context(self):
-        return {'default': self._default,
+        return {'prompt': self._prompt,
+                'error': self._error,
+                'default': self._default,
                 'options': self._options,
                 'attempts': self._attempts}
         
