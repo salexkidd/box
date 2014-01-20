14eb8b407cfb4978a03ca117227ce3eaad04cdc7
roll <roll@respect31.com>
2014-01-20 14:08:57 +0400

    refactored InputCall

--- box/input/input.py
+++ box/input/input.py
@@ -16,28 +16,28 @@ class InputCall:
     
     #Public
     
-    def __init__(self, prompt, error=None, 
+    def __init__(self, prompt=None, error=None,
                  default=None, options=None, attempts=None,
-                 prompt_template=None, error_template=None,
+                 formatted_prompt=None, formatted_error=None, 
                  input_function=None, print_function=None):
-        self._prompt = prompt
+        self._prompt = prompt or self._default_prompt
         self._error = error or self._default_error
         self._default = default
         self._options = options
         self._attempts = attempts or self._default_attempts
-        self._prompt_template = prompt_template or self._default_prompt_template
-        self._error_template = error_template or self._default_error_template
+        self._formatted_prompt = formatted_prompt or self._default_formatted_prompt
+        self._formatted_error = formatted_error or self._default_formatted_error
         self._input_function = input_function or self._default_input_function
         self._print_function = print_function or self._default_print_function
         
     def execute(self):
         for _ in range(0, self._attempts):
-            result = self._input_function(self._rendered_prompt)
+            result = self._input_function(self._prepared_prompt)
             if not result:
                 result = self._default
             if self._options:
                 if result not in self._options:
-                    self._print_function(self._rendered_error)
+                    self._print_function(self._prepared_error)
                     continue 
             return result
         else:
@@ -47,48 +47,35 @@ class InputCall:
                     attempts=self._attempts,
                     options=self._options))       
     
-    #Protected
+    def format_prompt(self):
+        return self._prompt+':'  
+
+    def format_error(self):
+        return self._error
     
+    #Protected
+
+    _default_prompt = 'Input'
     _default_error = 'Try again..'
-    _default_attempts = 3
-    _default_prompt_template = '''{{ prompt }}
-        {%- if options -%}
-          [{% for option in options %}
-            {%- if option == default %}
-              {{ option|upper }}/
-            {% else %}
-              {{ option }}/
-            {% endif %}
-          {% endfor %}]
-        {%- endif %}:'''
-    _default_error_template = '{{ error }}'       
+    _default_attempts = 3 
+    _default_formatted_prompt = staticmethod(format_prompt)
+    _default_formatted_error = staticmethod(format_error)
     _default_input_function = staticmethod(input)
     _default_print_function = staticmethod(print)
-        
-    @property
-    def _rendered_prompt(self):
-        template = self._template_class(self._prompt_template)
-        rendered_prompt = template.render(self._context)
-        return rendered_prompt
+     
+    @property   
+    def _prepared_prompt(self):
+        prepared_prompt = self._formatted_prompt
+        if callable(prepared_prompt):
+            prepared_prompt = prepared_prompt(self)
+        return prepared_prompt
 
-    @property
-    def _rendered_error(self):
-        template = self._template_class(self._error_template)
-        rendered_error = template.render(self._context)
-        return rendered_error
-    
-    @property
-    def _context(self):
-        return {'prompt': self._prompt,
-                'error': self._error,
-                'default': self._default,
-                'options': self._options,
-                'attempts': self._attempts}
-        
-    @property
-    def _template_class(self):
-        from jinja2 import Template
-        return Template
+    @property   
+    def _prepared_error(self):
+        prepared_error = self._formatted_error
+        if callable(prepared_error):
+            prepared_error = prepared_error(self)
+        return prepared_error
     
     
 locals().update({'input': Input()}) 
