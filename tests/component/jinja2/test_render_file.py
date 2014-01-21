ae60c5ecbad08297854e5d36dc2d4aff032e6300
roll <roll@respect31.com>
2014-01-21 13:11:35 +0400

    refactored render_fileTest

--- tests/component/jinja2/test_render_file.py
+++ tests/component/jinja2/test_render_file.py
@@ -1,43 +1,49 @@
 import unittest
 from functools import partial
 from unittest.mock import Mock, mock_open
-from box.jinja2.render_file import RenderFileCall
+from box.jinja2.render_file import render_file
 
-class RenderFileCallTest(unittest.TestCase):
+class render_fileTest(unittest.TestCase):
 
     #Public
 
     def setUp(self):
         self.template = Mock(render=Mock(return_value='text'))
-        MockCall = (self._make_mock_call_class(self.template))
-        self.partial_call = partial(MockCall, '/dirpath/filename')
+        self.mock_render = self._make_mock_render_function(self.template)
+        self.partial_render = partial(self.mock_render, '/dirpath/filename')
         
-    def test_execute(self):
-        call = self.partial_call()
-        self.assertEqual(call.execute(), 'text')
-        call._file_system_loader_class.assert_called_with('/dirpath')
-        call._environment_class.assert_called_with(loader='loader')
-        (call._environment_class.return_value.get_template.
+    def test(self):
+        result = self.partial_render()
+        self.assertEqual(result, 'text')
+        (self.mock_render._call_class._file_system_loader_class.
+            assert_called_with('/dirpath'))
+        (self.mock_render._call_class._environment_class.
+            assert_called_with(loader='loader'))
+        (self.mock_render._call_class._environment_class.
+         return_value.get_template.
             assert_called_with('filename'))
         self.template.render.assert_called_with({})
     
-    def test_execute_with_target(self):
-        call = self.partial_call(target='/target')
-        self.assertEqual(call.execute(), 'text')
-        call._open_function.assert_called_with('/target', 'w')
-        call._open_function().write.assert_called_with('text')
+    def test_with_target(self):
+        result = self.partial_render(target='/target')
+        self.assertEqual(result, 'text')
+        (self.mock_render._call_class._open_function.
+            assert_called_with('/target', 'w'))
+        (self.mock_render._call_class._open_function().write.
+            assert_called_with('text'))
         
-    def test_execute_with_context_is_object(self):
+    def test_with_context_is_object(self):
         context = object()
-        call = self.partial_call(context) 
-        self.assertEqual(call.execute(), 'text')
-        call._object_context_class.assert_called_with(context)
+        result = self.partial_render(context)
+        self.assertEqual(result, 'text')
+        (self.mock_render._call_class._object_context_class.
+            assert_called_with(context))
         self.template.render.assert_called_with('object_context')   
     
     #Protected
     
-    def _make_mock_call_class(self, template):
-        class MockCall(RenderFileCall):
+    def _make_mock_render_function(self, template):
+        class MockRenderCall(render_file._call_class):
             #Public
             meta_module = 'module'
             #Protected
@@ -47,4 +53,7 @@ class RenderFileCallTest(unittest.TestCase):
                 get_template=Mock(return_value=template)))
             _file_system_loader_class = Mock(return_value='loader')
             _object_template_class = Mock()
-        return MockCall
+        class MockRender(type(render_file)):
+            #Protected
+            _call_class = MockRenderCall
+        return MockRender()           
