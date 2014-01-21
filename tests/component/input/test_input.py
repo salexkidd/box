6e9f577469a78a16a884b3cea72f5d454b501d02
roll <roll@respect31.com>
2014-01-21 12:13:05 +0400

    refactored inputTest

--- tests/component/input/test_input.py
+++ tests/component/input/test_input.py
@@ -1,46 +1,46 @@
 import unittest
 from functools import partial
 from unittest.mock import Mock
-from box.input.input import InputCall
+from box.input.input import input
 
-class InputCallTest(unittest.TestCase):
+class inputTest(unittest.TestCase):
     
     #Public
     
     def setUp(self):
-        self.partial_call = partial(InputCall, 'prompt')
+        self.partial_input = partial(input, 'prompt')
     
-    def test_execute(self):
+    def test_input(self):
         input_function = Mock(return_value='input')
-        call = self.partial_call(
+        result = self.partial_input(
             input_function=input_function)
-        self.assertEqual(call.execute(), 'input')
+        self.assertEqual(result, 'input')
         input_function.assert_called_with('prompt:')
         
-    def test_execute_with_default(self):
+    def test_input_with_default(self):
         input_function = Mock(return_value='')
-        call = self.partial_call(
+        result = self.partial_input(
             default='default', 
             input_function=input_function)
-        self.assertEqual(call.execute(), 'default')
+        self.assertEqual(result, 'default')
         input_function.assert_called_with('prompt [default]:')
     
-    def test_execute_with_options(self):
+    def test_input_with_options(self):
         input_function = Mock(return_value='')
         print_function = Mock()
-        call = self.partial_call(
+        self.assertRaises(ValueError, 
+            self.partial_input, 
             options=['y', 'n'], 
             input_function=input_function,
             print_function=print_function)
-        self.assertRaises(ValueError, call.execute)
         input_function.assert_called_with('prompt [y/n]:')
         print_function.assert_called_with('Try again..')
         
-    def test_execute_with_default_and_options(self):
+    def test_input_with_default_and_options(self):
         input_function = Mock(return_value='')
-        call = self.partial_call(
+        result = self.partial_input(
             default='y',
             options=['y', 'n'], 
             input_function=input_function)
-        self.assertEqual(call.execute(), 'y')
+        self.assertEqual(result, 'y')
         input_function.assert_called_with('prompt [Y/n]:')
