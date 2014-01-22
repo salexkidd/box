394edde2cc00c3a80ae3e10014bb656bcaf1a9e3
roll <roll@respect31.com>
2014-01-22 20:50:12 +0400

    implemented new MapReduce

--- box/itertools/map_reduce.py
+++ box/itertools/map_reduce.py
@@ -1,79 +1,82 @@
 class MapReduce:
     """MapReduce function-class"""
     
-    BREAK_BEFORE = 1
-    BREAK_AFTER = 2
-    
-    def __call__(self, iterable, 
-                 breakers=[], filters=[], processors=[], reducers=[]):
-        elements = self._map(iterable, breakers, filters, processors)
-        reduced_elements = self._reduce(elements, reducers)
-        return reduced_elements
+    def __call__(self, iterable, mappers=[], reducers=[]):
+        values = self._map(iterable, mappers)
+        reduced_values = self._reduce(values, reducers)
+        return reduced_values
 
     #Protected
     
-    def _map(self, iterable, breakers, filters, processors):
-        for element, *args in iterable:
-            is_break = self._break(breakers, element, *args)
-            if is_break == self.BREAK_BEFORE:
-                break
-            if not self._filter(filters, element, *args):
+    def _map(self, iterable, mappers):
+        for emitter in iterable:
+            if not isinstance(emitter, MapEmmiter):
+                emitter = MapEmmiter(emitter)
+            for mapper in mappers:
+                mapper(emitter)
+            if emitter.skipped:
                 continue
-            processed_element = self._process(processors, element, *args)
-            yield processed_element
-            if is_break == self.BREAK_AFTER:
+            for value in emitter.emitted:
+                yield value
+            else:
+                yield emitter.get_value()
+            if emitter.stopped:
                 break
     
-    def _reduce(self, elements, reducers):
-        result = elements
+    def _reduce(self, values, reducers):
+        result = values
         for reducer in reducers:
             result = reducer(result)
         return result
     
-    def _break(self, breakers, element, *args):
-        for breaker in breakers:
-            is_break = breaker(element, *args)
-            if is_break:
-                return is_break
-        return False 
-    
-    def _filter(self, filters, element, *args):
-        for fltr in filters:
-            if not fltr(element, *args):
-                return False
-        return True
-    
-    def _process(self, processors, element, *args):
-        result = element
-        for processor in processors:
-            result = processor(result, *args)
-        return result
-    
 
-class MapEmitter:
+class MapEmmiter:
 
     #Public
 
-    def __init__(self, **kwargs):
-        for key, value in kwargs:
-            setattr(self, key, value)
+    def __init__(self, value, **context):
+        self._value = value
+        self._context = context
         self._emitted = []
+        self._skipped = False
         self._stopped = False
             
-    def emit(self, value):
-        self._emitted.append(value)
-        
-    def stop(self):
-        self._stopped = True
-        
+    def __getattr__(self, name):
+        try:
+            return self._context[name]
+        except KeyError:
+            raise AttributeError(name)
+            
+    def get_value(self):
+        return self._value               
+    
+    def set_value(self, value, condition=None):
+        if condition == None or condition:
+            self._value = value
+                
+    def emit(self, value, condition=None):
+        if condition == None or condition:
+            self._emitted.append(value)
+            
+    def skip(self, condition=None):
+        if condition == None or condition:
+            self._skipped = True
+           
+    def stop(self, condition=None):
+        if condition == None or condition:
+            self._stopped = True
+    
     @property
     def emitted(self):
         return self._emitted
     
     @property
+    def skipped(self):
+        return self._skipped
+    
+    @property
     def stopped(self):
-        return self._stopped    
-
+        return self._stopped
 
 
 map_reduce = MapReduce()
