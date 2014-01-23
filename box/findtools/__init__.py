a8aa146836030fa79333746ea8c1d04be9205ede
roll <roll@respect31.com>
2014-01-23 11:28:18 +0400

    updated findtools API

--- box/findtools/__init__.py
+++ box/findtools/__init__.py
@@ -1,4 +1,5 @@
 from .find_files import (FindFiles, FindFilesCall, FindFilesMaxDepthMapper, 
                          FindFilesNameMapper, find_files)
-from .find_strings import FindStrings, find_strings
-from .find_objects import FindObjects, FindObjectsNameFilter, find_objects
+from .find_strings import FindStrings, FindStringsCall, find_strings
+from .find_objects import (FindObjects, FindObjectsCall, 
+                           FindObjectsNameMapper, find_objects)
