d91f8f523614dd587e3b91342f91b95b6bee843a
roll <roll@respect31.com>
2014-02-12 11:44:01 +0400

    implemented FunctionMetaclass, FunctionCall

--- box/functools/call.py
+++ box/functools/call.py
@@ -1,5 +1,19 @@
-class FunctionCall:
+from abc import ABCMeta, abstractmethod
+
+class FunctionMetaclass(ABCMeta):
+    
+    #Public
+    
+    def __call__(self, *args, **kwargs):
+        function = super().__call__(*args, **kwargs)
+        result = function.invoke()
+        return result
+
+
+class FunctionCall(metaclass=FunctionMetaclass):
 
     #Public
-
-    pass
+    
+    @abstractmethod    
+    def invoke(self):
+        pass #pragma: no cover
