e1c048e85d54e5e50afe1d7a1f4e1c97404b9854
roll <roll@respect31.com>
2014-02-15 15:27:56 +0400

    added find_file/string/object stubs

--- box/findtools/__init__.py
+++ box/findtools/__init__.py
@@ -1,6 +1,9 @@
+from .find_file import find_file
 from .find_files import (find_files, FindFilesMapEmitter,
                          FindFilesMaxdepthMapper, FindFilesFilenameMapper, 
                          FindFilesFilepathMapper)
+from .find_string import find_string
 from .find_strings import find_strings, FindStringsMapEmitter
+from .find_object import find_object
 from .find_objects import (find_objects, FindObjectsMapEmitter,
                            FindObjectsObjnameMapper, FindObjectsObjtypeMapper)
