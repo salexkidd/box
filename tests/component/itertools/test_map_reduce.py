1647cb9e11cf51fb79fbe41edec0976c48a36551
roll <roll@respect31.com>
2014-02-16 10:10:12 +0400

    min

--- tests/component/itertools/test_map_reduce.py
+++ tests/component/itertools/test_map_reduce.py
@@ -9,41 +9,41 @@ class map_reduce_Test(unittest.TestCase):
         self.iterable = ['value1', 'value2']
         
     def test(self):
-        values = list(map_reduce(self.iterable))
-        self.assertEqual(values, ['value1', 'value2'])
+        result = list(map_reduce(self.iterable))
+        self.assertEqual(result, ['value1', 'value2'])
         
     def test_with_mapper_using_set_and_get_value(self):
         mapper = lambda emitter: emitter.value(emitter.value()+'!')
-        values = list(map_reduce(self.iterable, mappers=[mapper]))
-        self.assertEqual(values, ['value1!', 'value2!'])
+        result = list(map_reduce(self.iterable, mappers=[mapper]))
+        self.assertEqual(result, ['value1!', 'value2!'])
     
     def test_with_mapper_using_emit_with_condition(self):
         mapper1 = lambda emitter: emitter.emit('emitted1')
         mapper2 = (lambda emitter: 
             emitter.emit('emitted2', emitter.value() == 'value1'))
-        values = list(map_reduce(self.iterable, mappers=[mapper1, mapper2]))
-        self.assertEqual(values, ['emitted1', 'emitted2', 'emitted1'])
+        result = list(map_reduce(self.iterable, mappers=[mapper1, mapper2]))
+        self.assertEqual(result, ['emitted1', 'emitted2', 'emitted1'])
            
     def test_with_mapper_using_skip(self):
         mapper = lambda emitter: emitter.skip()
-        values = list(map_reduce(self.iterable, mappers=[mapper]))
-        self.assertEqual(values, [])
+        result = list(map_reduce(self.iterable, mappers=[mapper]))
+        self.assertEqual(result, [])
         
     def test_with_mapper_using_stop(self):
         mapper = lambda emitter: emitter.stop()
-        values = list(map_reduce(self.iterable, mappers=[mapper]))
-        self.assertEqual(values, ['value1'])                 
+        result = list(map_reduce(self.iterable, mappers=[mapper]))
+        self.assertEqual(result, ['value1'])                 
         
     def test_with_reducer(self):
-        reducer = lambda values: 'reduced'
-        values = map_reduce(self.iterable, reducers=[reducer])
-        self.assertEqual(values, 'reduced')
+        reducer = lambda result: 'reduced'
+        result = map_reduce(self.iterable, reducers=[reducer])
+        self.assertEqual(result, 'reduced')
         
     def test_with_reducer_and_fallback(self):
-        reducer = lambda values: 1/0
-        values = map_reduce(self.iterable, 
+        reducer = lambda result: 1/0
+        result = map_reduce(self.iterable, 
             reducers=[reducer], fallback='fallback')
-        self.assertEqual(values, 'fallback')        
+        self.assertEqual(result, 'fallback')        
 
 
 class MapEmitterTest(unittest.TestCase): 
