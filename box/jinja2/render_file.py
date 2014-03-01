7317f6c777858e3d49a7f7428703b0b09d56319b
roll <roll@respect31.com>
2014-03-01 10:09:39 +0400

    implemented render_string, rebased render_file on render_string

--- box/jinja2/render_file.py
+++ box/jinja2/render_file.py
@@ -1,64 +1,22 @@
 import os
-from ..functools import cachedproperty, FunctionCall 
-from .environment import EnvironmentMixin
+from ..functools import cachedproperty 
+from .render_string import render_string
 
-class render_file(FunctionCall):
+class render_file(render_string):
     
-    #Public
-    
-    def __init__(self, source, context={}, *, loader=None, target=None):
-        self._source = source
-        self._context = context
-        self._target = target
-        self._loader = loader
-    
-    def __call__(self):
-        content = self._render()
-        self._write(content)
-        return content
-            
     #Protected
-    
-    _open_function = staticmethod(open)
-    
-    def _render(self):
-        return self._template.render(self._context)
-    
-    def _write(self, content):
-        if self._target:
-            with self._open_function(self._target, 'w') as file:
-                file.write(content)
                 
     @cachedproperty
     def _template(self):
-        return self._environment.get_template(self._effective_source)    
-    
-    @cachedproperty
-    def _environment(self):
-        return self._environment_class(loader=self._effective_loader)
-    
-    @cachedproperty
-    def _effective_source(self):
-        if self._loader:
-            return self._source
-        else:
-            return os.path.basename(self._source)
-            
-    @cachedproperty
-    def _effective_loader(self):
-        if self._loader:
-            return self._loader
-        else:
-            dirpath = os.path.dirname(self._source)
-            return self._file_system_loader_class(dirpath)
+        source = self._source
+        loader = self._loader
+        if not loader:
+            dirpath, source = os.path.split(self._source)
+            loader = self._file_system_loader_class(dirpath)
+        environment = self._environment_class(loader=loader)          
+        return environment.get_template(source)
     
     @property
     def _file_system_loader_class(self):
         from jinja2 import FileSystemLoader
         return FileSystemLoader
-    
-    @property
-    def _environment_class(self):
-        from jinja2 import Environment
-        class Environment(EnvironmentMixin, Environment): pass
-        return Environment
--- box/jinja2/render_string.py
+++ box/jinja2/render_string.py
@@ -1,7 +1,40 @@
-from ..functools import FunctionCall
+from ..functools import cachedproperty, FunctionCall 
+from .environment import EnvironmentMixin
 
 class render_string(FunctionCall):
     
     #Public
     
-    pass
+    def __init__(self, source, context={}, *, loader=None, target=None):
+        self._source = source
+        self._context = context
+        self._target = target
+        self._loader = loader
+    
+    def __call__(self):
+        content = self._render()
+        self._write(content)
+        return content
+            
+    #Protected
+    
+    _open_function = staticmethod(open)
+    
+    def _render(self):
+        return self._template.render(self._context)
+    
+    def _write(self, content):
+        if self._target:
+            with self._open_function(self._target, 'w') as file:
+                file.write(content)
+                
+    @cachedproperty
+    def _template(self):
+        environment = self._environment_class(loader=self._loader)
+        return environment.from_string(self._source)
+    
+    @property
+    def _environment_class(self):
+        from jinja2 import Environment
+        class Environment(EnvironmentMixin, Environment): pass
+        return Environment
