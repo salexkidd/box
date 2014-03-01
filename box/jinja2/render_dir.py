90a72e4fd4b0fa299ef029b02c9c4747e3704010
roll <roll@respect31.com>
2014-03-01 14:30:31 +0400

    implemented first version of render_dir

--- box/jinja2/render_dir.py
+++ box/jinja2/render_dir.py
@@ -1,7 +1,23 @@
+import os
 from ..functools import FunctionCall
+from .render_string import render_string
 
 class render_dir(FunctionCall):
     
     #Public
     
-    pass
+    def __init__(self, source, context={}):
+        self._source = source
+        self._context = context
+    
+    def __call__(self):
+        for name in os.listdir(self._source):
+            try:
+                new_name = render_string(name, self._context)
+                if name != new_name:
+                    path = os.path.join(self._source, name)
+                    new_path = os.path.join(self._source, new_name)
+                    os.rename(path, new_path)
+            except Exception:
+                pass
+            
