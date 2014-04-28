e4831ababb6aaf976e488a0363db274ef10a8ed5
roll <roll@respect31.com>
2014-04-28 10:48:56 +0200

    another try to deploy

--- runfile.py
+++ runfile.py
@@ -23,7 +23,7 @@ class MainModule(PythonPackgramModule):
     maintainer = 'roll'
     maintainer_email = 'roll@respect31.com'
     name = 'box'
-    platforms = ['Unix']    
+    platforms = ['Unix']
     pypi_user = 'roll'
     pypi_password_secure = 'jFRWJAyhP5RA9j8CEpJzwJhwZmbfcxW1HpRO43mMu2/Nh3FW7GdisUovCLOS/khygJvh86vfe6m69+GEVuH/VgUEw8GcdZ41Zcla0ZnBhQrH0PTQel5Fou85foD7yXf42toVY3DV7C/JPk8PK3swSlIJ26n4dVreI7y1xvRmVHk='
     tests_require = ['nose']
