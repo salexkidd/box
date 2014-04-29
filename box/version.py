b551aff7686fc046d213f3078689dcbd534e8898
roll <roll@respect31.com>
2014-04-29 13:23:53 +0200

    Revert "broke version to check travis build fail"
    This reverts commit e4a3edb0f1535aa462614beeb5e33f4e7de9fc0f.

--- box/version.py
+++ box/version.py
@@ -32,4 +32,4 @@ class Version(str):
         return (self.major, self.minor, self.micro, self.level, self.serial)
     
     
-version = Version(major=0, minor=15, micro=1)
+version = Version(major=0, minor=15, micro=0)
