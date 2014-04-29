23c62658de4efaf1e9bc08b9921c4792f145a456
roll <roll@respect31.com>
2014-04-29 11:38:06 +0200

    improved balanced_walk test

--- tests/integration/os/balanced_walk/test_balanced_walk.py
+++ tests/integration/os/balanced_walk/test_balanced_walk.py
@@ -6,8 +6,14 @@ class balanced_walk_Test(unittest.TestCase):
 
     #Public
 
-    def test(self):
-        print(list(balanced_walk(self._get_fixtures_path())))
+    def test_find(self):
+        files = list(list(balanced_walk(self._get_fixtures_path())))
+        self.assertEqual(files, [
+            self._get_fixtures_path('file1'),
+            self._get_fixtures_path('file2'), 
+            self._get_fixtures_path('folder1', 'file1'),
+            self._get_fixtures_path('folder2', 'file1'),
+            self._get_fixtures_path('folder1', 'subfolder1', 'file1'),])        
         
     #Protected
     
