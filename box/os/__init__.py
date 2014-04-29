abf7042cc7ef5f8eb4b7ec5422574a620ea20abd
roll <roll@respect31.com>
2014-04-29 10:58:59 +0200

    implemented balanced_walk

---
+++ box/os/__init__.py
@@ -0,0 +1,1 @@
+from .balanced_walk import balanced_walk
--- box/os/balanced_walk.py
+++ box/os/balanced_walk.py
@@ -1,2 +1,25 @@
-def balanced_walk():
-    pass
+import os
+
+#TODO: add onerror
+def balanced_walk(dirpathes):
+    """Recursevly yield filepathes from top to bottom of directory tree
+    
+    Function doesn't support symbolic links.
+    
+    :param list dirpathes: paths of top directories 
+    :returns generator: filepath generator
+    """
+    for dirpath in dirpathes:
+        inner_filepathes = []        
+        inner_dirpathes = []
+        for path in os.listdir(dirpath):
+            if os.path.islink(path):
+                continue
+            elif os.path.isfile(path):
+                inner_filepathes.append(path)            
+            elif os.path.isdir(path):
+                inner_dirpathes.append(path)         
+        inner_filepathes.sort()
+        inner_dirpathes.sort()
+        yield from inner_filepathes
+        yield from balanced_walk(inner_dirpathes)
