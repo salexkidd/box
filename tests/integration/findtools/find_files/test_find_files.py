cdd3c0a7a44e9b03cf6e95ef4420a3494380c5d2
roll <roll@respect31.com>
2014-04-29 09:19:12 +0200

    added os.walk/maxdepth bringing out tests

--- tests/integration/findtools/find_files/test_find_files.py
+++ tests/integration/findtools/find_files/test_find_files.py
@@ -5,22 +5,23 @@ from box.findtools import find_files
 class find_files_Test(unittest.TestCase):
 
     #Public
-    
-    def setUp(self):
-        self.basedir = self._get_fixtures_path() 
 
     def test_find(self):
         files = list(find_files(
-            filename='file1', basedir=self.basedir))
+            filename='file1', basedir=self._get_fixtures_path()))
         self.assertEqual(files, [
             self._get_fixtures_path('file1'), 
-            self._get_fixtures_path('folder', 'file1')])
+            self._get_fixtures_path('folder1', 'file1'),
+            self._get_fixtures_path('folder2', 'file1'),
+            self._get_fixtures_path('folder1', 'subfolder1', 'file1'),])
 
-    def test_find_with_maxdepth_is_1(self):
+    def test_find_with_maxdepth_is_2(self):
         files = list(find_files(
-            filename='file1', basedir=self.basedir, maxdepth=1))
+            filename='file1', basedir=self._get_fixtures_path(), maxdepth=2))
         self.assertEqual(files, [
-            self._get_fixtures_path('file1')])
+            self._get_fixtures_path('file1'), 
+            self._get_fixtures_path('folder1', 'file1'),
+            self._get_fixtures_path('folder2', 'file1'),])
         
     #Protected    
         
