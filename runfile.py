c61514e8c83aa3811fed6742bbfa3774a513dbb7
roll <roll@respect31.com>
2014-05-02 09:37:01 +0200

    added sphinx to dev requires

--- runfile.py
+++ runfile.py
@@ -18,7 +18,7 @@ class MainModule(PythonPackgramModule):
         'Topic :: System :: Systems Administration', 
     ]
     description = 'Box is a library to provide common functionality.'
-    development_requires = []    
+    development_requires = ['Sphinx>=1.2']    
     github_user = 'respect31'
     install_requires = []
     license = 'MIT License'
