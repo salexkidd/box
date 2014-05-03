07fdec4f5eafe2591693899ad80a8175934a7e83
roll <roll@respect31.com>
2014-05-03 09:16:42 +0200

    min

--- box/importlib/import_object.py
+++ box/importlib/import_object.py
@@ -4,19 +4,26 @@ def import_object(name, package=None):
     """Import an object.
     
     :param str/mixed name: 
-      object name in 'module.attr' form
+      object name in "[module.]module.attr" form
     
     :param object package: argument is required when performing 
       a relative import. It specifies the package to use as the anchor point
       from which to resolve the relative import to an absolute import
       
     :raises ValueError: 
-      if name is a string but not in 'module.attr' form
+      if name is a string but not in proper form
       
     :returns object/mixed: 
       object by name
       
-    If name not is a string function just returns name without changes.
+    If name not is a string function returns name without changes.
+    It usefull when client may give you pointer to some objects in 
+    two forms like string or already imported object:
+    
+    >>> obj = import_object('box.importlib.import_object')
+    >>> obj is import_object(obj)
+    >>> obj
+    <function box.importlib.import_object.import_object>
     """
     if isinstance(name, str):
         try:
