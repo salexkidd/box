b1b77c15e18ee56b586e32636c6f5792f2802de7
roll <roll@respect31.com>
2014-05-04 19:40:49 +0200

    min

--- tests/integration/findtools/find_files/test_find_files.py
+++ tests/integration/findtools/find_files/test_find_files.py
@@ -12,17 +12,17 @@ class find_files_Test(unittest.TestCase):
             filename='file1', basedir=self._get_fixtures_path()))
         self.assertEqual(files, [
             self._get_fixtures_path('file1'), 
-            self._get_fixtures_path('folder1', 'file1'),
-            self._get_fixtures_path('folder2', 'file1'),
-            self._get_fixtures_path('folder1', 'subfolder1', 'file1'),])
+            self._get_fixtures_path('dir1', 'file1'),
+            self._get_fixtures_path('dir2', 'file1'),
+            self._get_fixtures_path('dir1', 'subdir1', 'file1'),])
 
     def test_find_with_maxdepth_is_2(self):
         files = list(find_files(
             filename='file1', basedir=self._get_fixtures_path(), maxdepth=2))
         self.assertEqual(files, [
             self._get_fixtures_path('file1'), 
-            self._get_fixtures_path('folder1', 'file1'),
-            self._get_fixtures_path('folder2', 'file1'),])
+            self._get_fixtures_path('dir1', 'file1'),
+            self._get_fixtures_path('dir2', 'file1'),])
         
     #Protected
         
