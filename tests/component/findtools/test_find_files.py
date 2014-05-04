9da8a3e555ef96f3f606a2fb7103b388d4cab7ac
roll <roll@respect31.com>
2014-05-04 19:43:15 +0200

    min

--- tests/component/findtools/test_find_files.py
+++ tests/component/findtools/test_find_files.py
@@ -10,8 +10,8 @@ class find_files_Test(unittest.TestCase):
     def setUp(self):
         walk_items = [
             'file1', 'file2', 
-            'folder/file1', 'folder/file2',
-            'folder/subfolder/file3',
+            'dir/file1', 'dir/file2',
+            'dir/subdir/file3',
         ]
         self.find = self._make_mock_find_function(walk_items)
    
@@ -30,11 +30,11 @@ class find_files_Test(unittest.TestCase):
         
     def test_with_maxdepth_is_2(self):
         files = list(self.find(filename='file1', maxdepth=2))
-        self.assertEqual(files, ['file1', 'folder/file1'])
+        self.assertEqual(files, ['file1', 'dir/file1'])
         
     def test_with_filename(self):
         files = list(self.find(filename='file3'))
-        self.assertEqual(files, ['folder/subfolder/file3'])        
+        self.assertEqual(files, ['dir/subdir/file3'])        
         
     def test_with_filename_is_regex(self):
         filename = re.compile('file1+')
@@ -42,18 +42,18 @@ class find_files_Test(unittest.TestCase):
         self.assertEqual(files, ['file1'])     
         
     def test_with_filepath(self):
-        files = list(self.find(filepath='folder/file*'))
-        self.assertEqual(files, ['folder/file1', 'folder/file2'])
+        files = list(self.find(filepath='dir/file*'))
+        self.assertEqual(files, ['dir/file1', 'dir/file2'])
         
     def test_with_filepath_is_regex(self):
         filepath = re.compile('.*2$')
         files = list(self.find(filepath=filepath))
-        self.assertEqual(files, ['file2', 'folder/file2'])        
+        self.assertEqual(files, ['file2', 'dir/file2'])        
         
     def test_with_mapper(self):
         mapper = lambda emitter: emitter.value(emitter.value()+'!')
         files = list(self.find(filename='file1', mappers=[mapper]))
-        self.assertEqual(files, ['file1!', 'folder/file1!'])         
+        self.assertEqual(files, ['file1!', 'dir/file1!'])         
           
     def test_with_reducer(self):
         reducer=lambda files: list(files)[0]
