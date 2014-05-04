39de99ac52721a191fbd066f60586b0202a2d444
roll <roll@respect31.com>
2014-05-04 19:38:54 +0200

    implemented component balanced_walk test

--- tests/component/os/test_balanced_walk.py
+++ tests/component/os/test_balanced_walk.py
@@ -1,9 +1,49 @@
+import re
 import unittest
+from unittest.mock import patch
 from box.os.balanced_walk import balanced_walk
 
 class balanced_walk_Test(unittest.TestCase):
 
     #Public
+    
+    def setUp(self):
+        patch('os.listdir', new=self._mock_listdir).start()
+        patch('os.path.islink', new=self._mock_islink).start()
+        patch('os.path.isfile', new=self._mock_isfile).start()
+        patch('os.path.isdir', new=self._mock_isdir).start()
+        patch('os.path.join', new=self._mock_join).start()
+        self.addCleanup(patch.stopall)    
 
     def test(self):
-        balanced_walk
+        files = list(balanced_walk('fixtures'))
+        self.assertEqual(files, [
+            'fixtures/file1',
+            'fixtures/file2', 
+            'fixtures/dir1/file1',
+            'fixtures/dir2/file1',
+            'fixtures/dir1/subdir1/file1',])
+        
+    #Protected
+   
+    def _mock_listdir(self, path):
+        if path == 'fixtures':
+            return ['dir1', 'dir2', 'file1', 'file2', 'link']
+        elif path == 'fixtures/dir1':
+            return ['subdir1', 'file1',]
+        elif path == 'fixtures/dir2':
+            return ['file1',]
+        elif path == 'fixtures/dir1/subdir1':
+            return ['file1']        
+    
+    def _mock_islink(self, path):
+        return bool(re.search('link\d?$', path))
+
+    def _mock_isfile(self, path):
+        return bool(re.search('file\d?$', path))
+    
+    def _mock_isdir(self, path):
+        return bool(re.search('dir\d?$', path))
+    
+    def _mock_join(self, *pathes):
+        return '/'.join(pathes)
