45cf9ea3786994f307181b7f1b54b71b8c54b844
roll <roll@respect31.com>
2014-05-08 10:40:45 +0200

    added wildcard test to find_files

--- tests/integration/findtools/find_files/test_find_files.py
+++ tests/integration/findtools/find_files/test_find_files.py
@@ -14,23 +14,29 @@ class find_files_Test(unittest.TestCase):
 
     def test_find(self):
         files = list(self.partial_find(filename='file1'))
-        self.assertEqual(files, [
-            'file1', 
-            'dir1/file1',
-            'dir2/file1',
-            'dir1/subdir1/file1'])
+        self.assertEqual(files, 
+            ['file1', 
+             'dir1/file1',
+             'dir2/file1',
+             'dir1/subdir1/file1'])
 
     def test_find_with_maxdepth_is_2(self):
         files = list(self.partial_find(filename='file1', maxdepth=2))
-        self.assertEqual(files, [
-            'file1', 
-            'dir1/file1',
-            'dir2/file1'])
+        self.assertEqual(files, 
+            ['file1', 
+             'dir1/file1',
+             'dir2/file1'])
     
     def test_find_with_filepath(self):
         files = list(self.partial_find(filepath='file1'))
-        self.assertEqual(files, [
-            'file1'])        
+        self.assertEqual(files, 
+            ['file1'])
+        
+    def test_find_with_filepath_wildcard(self):
+        files = list(self.partial_find(filepath='dir*/file*'))
+        self.assertEqual(files, 
+            ['dir1/file1',
+             'dir2/file1',])
         
     #Protected
     
