50f2c8481619198f374d8a325543d191419067d0
roll <roll@respect31.com>
2014-05-12 09:29:16 +0200

    upgraded to packgram 0.8

--- runfile.py
+++ runfile.py
@@ -1,5 +1,3 @@
-import re
-from run import copy
 from packgram.manage import PythonPackgramModule
     
 class MainModule(PythonPackgramModule):
@@ -20,7 +18,7 @@ class MainModule(PythonPackgramModule):
     description = 'Box is a library to provide common functionality.'
     development_requires = [
         'runpack>=0.14',
-        'packgram>=0.7',
+        'packgram>=0.8',
         'sphinx',
         'sphinx_rtd_theme',
     ]    
@@ -35,8 +33,3 @@ class MainModule(PythonPackgramModule):
     pypi_password_secure = 'jFRWJAyhP5RA9j8CEpJzwJhwZmbfcxW1HpRO43mMu2/Nh3FW7GdisUovCLOS/khygJvh86vfe6m69+GEVuH/VgUEw8GcdZ41Zcla0ZnBhQrH0PTQel5Fou85foD7yXf42toVY3DV7C/JPk8PK3swSlIJ26n4dVreI7y1xvRmVHk='
     tests_require = ['nose']
     test_suite = 'nose.collector'
-    version = copy(
-        PythonPackgramModule.version,
-        string=re.compile(
-            r'Version\(major=(\d*), minor=(\d*), micro=(\d*), level=\'(\w*)\'\)'),
-    )
