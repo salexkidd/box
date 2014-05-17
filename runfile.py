e8cd44a7564e00ae6e9de44bdd3a0bc24123f331
roll <roll@respect31.com>
2014-05-17 19:06:32 +0400

    remove inherited vars

--- runfile.py
+++ runfile.py
@@ -27,11 +27,8 @@ class MainModule(ManageModule):
     github_user = 'respect31'
     install_requires = []
     license = 'MIT License'
-    maintainer = 'roll'
-    maintainer_email = 'roll@respect31.com'
     name = 'box'
     platforms = ['Unix']
-    pypi_user = 'roll'
     pypi_password_secure = 'jFRWJAyhP5RA9j8CEpJzwJhwZmbfcxW1HpRO43mMu2/Nh3FW7GdisUovCLOS/khygJvh86vfe6m69+GEVuH/VgUEw8GcdZ41Zcla0ZnBhQrH0PTQel5Fou85foD7yXf42toVY3DV7C/JPk8PK3swSlIJ26n4dVreI7y1xvRmVHk='
     tests_require = ['packgram>=0.9', 'nose']
     test_suite = 'nose.collector'
