202e57045780575483bb12c08fd47fdbfa35fde8
roll <roll@respect31.com>
2014-05-21 08:41:41 +0400

    implemented render_file test

--- tests/integration/jinja2/render_file/test_render_file.py
+++ tests/integration/jinja2/render_file/test_render_file.py
@@ -1,13 +1,26 @@
 import os
 import unittest
+from functools import partial
+from box.jinja2 import render_file
 
 class render_file_Test(unittest.TestCase):
 
     #Public
-
+    
+    def setUp(self):
+        Context = self._make_mock_context_class()
+        self.prender = partial(render_file, context=Context())
+        
+    def test(self):
+        self.assertEqual(self.prender(self._make_path('file')), 'value1')
         
     #Protected
     
-    @property
-    def _basedir(self):
-        return os.path.join(os.path.dirname(__file__), 'fixtures') 
+    def _make_path(self, *args):
+        return os.path.join(os.path.dirname(__file__), 'fixtures', *args)
+    
+    def _make_mock_context_class(self):
+        class MockContext:
+            attr1 = 'value1'
+            attr2 = 'value2'
+        return MockContext
