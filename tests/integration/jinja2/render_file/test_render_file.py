aba59a68148b0c3a33fd60961a331064bbf966b3
roll <roll@respect31.com>
2014-05-21 08:47:24 +0400

    added template include to render_file test (error's reproduced)

--- tests/integration/jinja2/render_file/test_render_file.py
+++ tests/integration/jinja2/render_file/test_render_file.py
@@ -8,19 +8,17 @@ class render_file_Test(unittest.TestCase):
     #Public
     
     def setUp(self):
-        Context = self._make_mock_context_class()
+        class Context:
+            attr1 = 'value1'
+            attr2 = 'value2'
         self.prender = partial(render_file, context=Context())
         
     def test(self):
-        self.assertEqual(self.prender(self._make_path('file')), 'value1')
+        self.assertEqual(
+            self.prender(self._make_path('template1')), 
+            'value1\nvalue2')
         
     #Protected
     
     def _make_path(self, *args):
         return os.path.join(os.path.dirname(__file__), 'fixtures', *args)
-    
-    def _make_mock_context_class(self):
-        class MockContext:
-            attr1 = 'value1'
-            attr2 = 'value2'
-        return MockContext
