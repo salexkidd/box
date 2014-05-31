ef5fbec691c0f06ceeaa0e5e598d75cae74b797f
roll <roll@respect31.com>
2014-05-31 09:07:40 +0400

    Revert "added dict support for argparse arguments"
    This reverts commit b0e17d068f1831cf6d9bbc0e17072e181562dc2d.

--- box/argparse/command.py
+++ box/argparse/command.py
@@ -75,8 +75,6 @@ class Command:
     @property
     def _parser_arguments(self):
         arguments = self._config.get('arguments', [])
-        if isinstance(arguments, dict):
-            arguments = list(arguments.values())
         return arguments    
     
     @property
