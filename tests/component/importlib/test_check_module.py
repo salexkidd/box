aa675e31be07ffdb1a072dacbd405c13dbe4b877
roll <roll@respect31.com>
2014-06-02 18:47:21 +0400

    added check_module tests

---
+++ tests/component/importlib/test_check_module.py
@@ -0,0 +1,12 @@
+import unittest
+from box.importlib.check_module import check_module
+
+class check_module_Test(unittest.TestCase):
+
+    #Public
+
+    def test_module_is_available(self):
+        self.assertTrue(check_module('unittest'))
+    
+    def test_module_is_not_available(self):
+        self.assertFalse(check_module('not_available'))       
