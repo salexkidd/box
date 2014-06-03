e2c06d3755ee6d4ca0c3dcaf1e9a14428999f0ba
roll <roll@respect31.com>
2014-06-03 19:23:19 +0400

    improved merge_dicts

--- box/collections/merge_dicts.py
+++ box/collections/merge_dicts.py
@@ -1,26 +1,22 @@
 from ..copy import enhanced_copy
 
-def merge_dicts(dict1, dict2, *, mergers={}):
+def merge_dicts(dict1, dict2, *, resolvers={}):
     """Recursively merge dicts and return new one.
     
     :param dict dict1: left dict to merge
     :param dict dict2: right dict to merge
-    :param dict mergers: map of mergers by types
+    :param dict mergers: resolvers by type
     
     :returns dict: merged dict
     """
-    mergers.setdefault(dict, merge_dicts)
+    resolvers.setdefault(dict, merge_dicts)
     result = enhanced_copy(dict1)
     for key in dict2:
         value = dict2[key]
         if key in dict1:
-            value1 = dict1[key]
-            value2 = dict2[key]
-            type1 = type(value1)
-            type2 = type(value2)
-            if type1 == type2:
-                merger = mergers.get(type1, None)
-                if merger:
-                    value = merger(value1, value2)
+            resolver1 = resolvers.get(type(dict1[key]), None)
+            resolver2 = resolvers.get(type(dict2[key]), None)
+            if resolver1 == resolver2 != None:
+                value = resolver1(dict1[key], dict2[key])
         result[key] = value
     return result
--- tests/component/collections/test_merge_dicts.py
+++ tests/component/collections/test_merge_dicts.py
@@ -1,4 +1,5 @@
 import unittest
+import operator
 from box.collections.merge_dicts import merge_dicts
 
 class merge_dicts_Test(unittest.TestCase):
@@ -6,5 +7,31 @@ class merge_dicts_Test(unittest.TestCase):
     #Public
 
     def test(self):
-        result = merge_dicts({'a': 1}, {'b': 2})
-        self.assertEqual(result, {'a': 1, 'b': 2})
+        self.assertEqual(
+            merge_dicts(
+                {'a': 1},
+                {'b': 2}),
+            {'a': 1, 'b': 2})       
+        
+    def test_nested(self):
+        self.assertEqual(
+            merge_dicts(
+                {'a': 1, 'b': {'b1': 1, 'b2': 2}},
+                {'a': 2, 'b': {'b2': 1, 'b3': 2}}),
+            {'a': 2, 'b': {'b1': 1, 'b2': 1, 'b3': 2}})         
+        
+    def test_list_resolver(self):
+        self.assertEqual(
+            merge_dicts(
+                {'a': 1, 'b': [1, 2]},
+                {'a': 2, 'b': [3]},
+                resolvers={list: operator.add}),
+            {'a': 2, 'b': [1, 2, 3]})
+        
+    def test_int_and_float_resolver(self):
+        self.assertEqual(
+            merge_dicts(
+                {'a': 1, 'b': 3.0},
+                {'a': 2.0, 'b': 4},
+                resolvers={int: operator.mul, float: operator.mul}),
+            {'a': 2.0, 'b': 12.0})        
