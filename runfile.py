dcd72e2f6cacb8175afd401940a4a2ae6b74816a
roll <roll@respect31.com>
2014-06-04 14:27:51 +0400

    Revert "added sphinx to test_require"
    This reverts commit 3f58470d0ca660b2671034d1fec27baa011ae8e4.

--- runfile.py
+++ runfile.py
@@ -22,12 +22,12 @@ class MainModule(ManageModule):
         'Topic :: System :: Systems Administration', 
     ]
     description = 'Box is a library to provide common functionality.'
-    development_requires = ['packgram>=0.10.2', 'sphinx_rtd_theme']    
+    development_requires = ['packgram>=0.10.2', 'sphinx', 'sphinx_rtd_theme']    
     github_user = 'respect31'
     install_requires = []
     license = 'MIT License'
     name = 'box'
     platforms = ['Unix']
     pypi_password_secure = 'jFRWJAyhP5RA9j8CEpJzwJhwZmbfcxW1HpRO43mMu2/Nh3FW7GdisUovCLOS/khygJvh86vfe6m69+GEVuH/VgUEw8GcdZ41Zcla0ZnBhQrH0PTQel5Fou85foD7yXf42toVY3DV7C/JPk8PK3swSlIJ26n4dVreI7y1xvRmVHk='
-    tests_require = ['nose', 'jinja2', 'sphinx']
+    tests_require = ['nose', 'jinja2']
     test_suite = 'nose.collector'
