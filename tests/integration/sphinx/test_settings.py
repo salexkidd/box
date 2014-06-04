ebbc2837a23b1c2d0f66761603d5e3c3435da3f9
roll <roll@respect31.com>
2014-06-04 12:00:00 +0400

    added integration sphinx test

---
+++ tests/integration/sphinx/test_settings.py
@@ -0,0 +1,15 @@
+from box.importlib import check_module
+
+if check_module('sphinx'):
+    import unittest
+    from box.sphinx import Settings
+    
+    class SettingsTest(unittest.TestCase):
+    
+        #Public
+        
+        def setUp(self):
+            self.settings = Settings()
+    
+        def test_source_suffix(self):
+            self.assertEqual(self.settings.source_suffix, '.rst')
