c60ec82298180c42e5c5e3a0d2057ce6f345ae34
roll <roll@respect31.com>
2014-06-06 14:10:23 +0400

    added regex test

---
+++ tests/component/types/test_regex.py
@@ -0,0 +1,10 @@
+import re
+import unittest
+from box.types.regex import RegexCompiledPatternType
+
+class RegexCompiledPatternTypeTest(unittest.TestCase):
+
+    #Public
+
+    def test(self):
+        self.assertEqual(RegexCompiledPatternType, type(re.compile('')))
