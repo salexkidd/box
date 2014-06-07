eb795490f2863b22cef5c6d7c813ea9ad63d50db
roll <roll@respect31.com>
2014-06-07 09:46:44 +0400

    added not_emitted test

---
+++ tests/component/itertools/test_not_emitted.py
@@ -0,0 +1,14 @@
+import unittest
+from box.itertools.not_emitted import NotEmitted
+
+class NotEmittedTest(unittest.TestCase):
+
+    #Public
+
+    def test(self):
+        self.assertRaises(NotEmitted, self._raise)
+    
+    #Protected
+    
+    def _raise(self):
+        raise NotEmitted()
