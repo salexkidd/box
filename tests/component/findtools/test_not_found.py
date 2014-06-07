29f920b85acf36f23bdebf317c35ee9d30413c0c
roll <roll@respect31.com>
2014-06-07 11:00:41 +0400

    added NotFound test

---
+++ tests/component/findtools/test_not_found.py
@@ -0,0 +1,14 @@
+import unittest
+from box.findtools.not_found import NotFound
+
+class NotFoundTest(unittest.TestCase):
+
+    #Public
+
+    def test(self):
+        self.assertRaises(NotFound, self._raise)
+    
+    #Protected
+    
+    def _raise(self):
+        raise NotFound()
