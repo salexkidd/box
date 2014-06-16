e4ab9cec9ab2ff22c0cd65cdb440eb41fb1ebca9
roll <roll@respect31.com>
2014-06-16 20:27:56 +0400

    improved decorator tests

--- tests/component/decorator/test_decorator.py
+++ tests/component/decorator/test_decorator.py
@@ -4,29 +4,45 @@ from box.decorator.decorator import Decorator
 class DecoratorTest(unittest.TestCase):
 
     #Public
+    
+    def setUp(self):
+        decorator1 = self._make_mock_decorator1()
+        decorator2 = self._make_mock_decorator2()
+        client_class = self._make_client_class(decorator1, decorator2)
+        self.client = client_class()
 
-    def test(self):
-        decorator = self._make_mock_decorator()
-        self.assertEqual(decorator('method'), 'method')
+    def test_1_step(self):
+        self.assertEqual(self.client.method1(), 'method')
 
-    def test_with_init(self):
-        decorator = self._make_mock_decorator_with_init()
-        self.assertEqual(decorator('param')('method'), ('param', 'method'))        
+    def test_2_steps(self):
+        self.assertEqual(self.client.method2(), 'param')
     
     #Protected
     
-    def _make_mock_decorator(self):
-        class decorator(Decorator):
+    def _make_mock_decorator1(self):
+        class decorator1(Decorator):
             #Public
             def __call__(self, method):
                 return method
-        return decorator
+        return decorator1
             
-    def _make_mock_decorator_with_init(self):
-        class decorator(Decorator):
+    def _make_mock_decorator2(self):
+        class decorator2(Decorator):
             #Public
             def __init__(self, param):
                 self._param = param
             def __call__(self, method):
-                return (self._param, method)                
-        return decorator                     
+                method.param = self._param
+                return method                
+        return decorator2
+    
+    def _make_client_class(self, decorator1, decorator2):
+        class Client:
+            #Public
+            @decorator1
+            def method1(self):
+                return 'method'
+            @decorator2('param')
+            def method2(self):
+                return self.method2.param
+        return Client       
