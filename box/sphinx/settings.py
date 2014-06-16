921807a6ec4842e6aef86544a202ab388192ae0b
roll <roll@respect31.com>
2014-06-16 14:17:33 +0400

    added decorator docs

--- box/sphinx/settings.py
+++ box/sphinx/settings.py
@@ -76,19 +76,19 @@ class Settings(Settings):
         
     #Setup
     
-    def setup(self, app):
-        for name in sorted(dir(self)):
-            attr = getattr(self, name)
-            if isinstance(attr, setup):
-                attr.invoke(app)
+#     def setup(self, app):
+#         for name in sorted(dir(self)):
+#             attr = getattr(self, name)
+#             if isinstance(attr, setup):
+#                 attr.invoke(app)
         
     #Protected
     
-    @property
-    def _as_dict(self):
-        items = super()._as_dict
-        items['setup'] = self.setup
-        return items
+#     @property
+#     def _as_dict(self):
+#         items = super()._as_dict
+#         items['setup'] = self.setup
+#         return items
     
     @cachedproperty
     def _defaults(self):
---
+++ docs/api/decorator.rst
@@ -0,0 +1,21 @@
+=============
+box.decorator
+=============
+
+Module adds some decorator related functionality.
+
+------
+Public
+------
+
+Module’s public interface.
+
+.. autoclass:: box.decorator.Decorator
+
+--------
+Internal
+--------
+
+Module's internal implementation.
+
+.. autoclass:: box.decorator.decorator.DecoratorMetaclass
