b3dfa269ca97ac96af8518627100ffe204725765
roll <roll@respect31.com>
2014-06-16 21:20:35 +0400

    min

--- box/packtools/include.py
+++ box/packtools/include.py
@@ -1,13 +1,13 @@
 from ..decorator import Decorator
 
 class include(Decorator):
-    """Decorate function/method to be included in settings.
+    """Decorate object to be included in settings.
     """
 
     #Public
     
     attribute_name = '_box_packtools_include'
 
-    def __call__(self, function):
-        setattr(function, self.attribute_name, True)
-        return function
+    def __call__(self, obj):
+        setattr(obj, self.attribute_name, True)
+        return obj
