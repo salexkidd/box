c10174e465ce8ad88140c783644ac730d9e38998
roll <roll@respect31.com>
2014-06-22 13:23:07 +0400

    added module to import_object

--- box/importlib/import_object.py
+++ box/importlib/import_object.py
@@ -1,20 +1,24 @@
 import importlib
 
-def import_object(name, package=None):
+def import_object(name, *, module=None, package=None):
     """Import an object.
     
     :param str/mixed name: 
-      object name in "[module.]module.attr" form
+      object name in "[module.]module.]attr" form
+      
+    :param str module:
+      if argument is passed name will be processed as just an attribute
     
-    :param object package: argument is required when performing 
-      a relative import. It specifies the package to use as the anchor point
-      from which to resolve the relative import to an absolute import
+    :param str package: 
+      argument is required when performing a relative import. 
+      It specifies the package to use as the anchor point from which 
+      to resolve the relative import to an absolute import
       
     :raises ValueError: 
-      if name is a string but not in proper form
+      if name is a string but not in a proper form
       
     :returns object/mixed: 
-      object by name
+      imported object
       
     If name not is a string function returns name without changes.
     It usefull when client may give you pointer to some objects in 
@@ -26,12 +30,13 @@ def import_object(name, package=None):
       <function box.importlib.import_object.import_object>
     """
     if isinstance(name, str):
-        try:
-            module_name, attribute_name = name.rsplit('.', 1)
-        except ValueError:
-            raise ValueError('Name format not is [.]module.attr')
-        module = importlib.import_module(module_name, package)
-        attribute = getattr(module, attribute_name)
+        if module == None:
+            try:
+                module, name = name.rsplit('.', 1)
+            except ValueError:
+                raise ValueError('Name is in a bad form.') from None
+        module = importlib.import_module(module, package=package)
+        attribute = getattr(module, name)
     else:
         attribute = name
     return attribute
