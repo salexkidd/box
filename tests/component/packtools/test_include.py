7f8bb357dd2b9eecee143aae735fa6cf773c9f78
roll <roll@respect31.com>
2014-07-08 19:01:39 +0400

    min

--- tests/component/packtools/test_include.py
+++ tests/component/packtools/test_include.py
@@ -1,10 +1,14 @@
 import unittest
+from unittest.mock import Mock
 from box.packtools.include import include
 
 class include_Test(unittest.TestCase):
 
     #Public
+    
+    def setUp(self):
+        self.callable = Mock()
+        self.callable= include(self.callable)
 
     def test(self):
-        include(self)
-        self.assertTrue(getattr(self, include.attribute_name))
+        self.assertTrue(getattr(self.callable, include.attribute_name))
--- tests/component/sphinx/test_setup.py
+++ tests/component/sphinx/test_setup.py
@@ -1,9 +1,14 @@
 import unittest
-#from box.sphinx.setup import setup
+from unittest.mock import Mock
+from box.sphinx.setup import setup as sphinx_setup
 
 class setup_Test(unittest.TestCase):
 
     #Public
+    
+    def setUp(self):
+        self.method = Mock()
+        sphinx_setup(self.method)
 
     def test(self):
-        pass
+        sphinx_setup
