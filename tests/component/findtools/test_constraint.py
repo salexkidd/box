8dec1b971096413ccc34424d2fb006564f1ff46f
roll <roll@respect31.com>
2014-07-08 19:46:26 +0400

    added PatternConstraint test stub

---
+++ tests/component/findtools/test_constraint.py
@@ -0,0 +1,12 @@
+import unittest
+from box.findtools.constraint import PatternConstraint
+
+class PatternConstraintTest(unittest.TestCase):
+
+    #Public
+    
+    def setUp(self):
+        pass
+
+    def test(self):
+        pass
