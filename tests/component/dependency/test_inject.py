e4e90ed011ccbe1f2a81e9be9b54fb0e2c5490fe
roll <roll@respect31.com>
2014-07-08 13:17:20 +0400

    added inject test

---
+++ tests/component/dependency/test_inject.py
@@ -0,0 +1,22 @@
+import unittest
+from unittest.mock import Mock
+from box.dependency.inject import inject
+
+class inject_Test(unittest.TestCase):
+
+    #Public
+    
+    def setUp(self):
+        self.Client = self._make_client_class()
+        self.client = self.Client()
+
+    def test(self):
+        self.assertEqual(self.client.injection, Mock)
+    
+    #Protected
+    
+    def _make_client_class(self):
+        class Client:
+            #Public
+            injection = inject('unittest.mock.Mock')
+        return Client
