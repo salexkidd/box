21a5bb4000a10343d396eba8dad667fb8bee1057
roll <roll@respect31.com>
2014-07-09 10:08:56 +0400

    added bool, repr test to constraint

--- tests/component/findtools/test_constraint.py
+++ tests/component/findtools/test_constraint.py
@@ -6,7 +6,26 @@ class PatternConstraintTest(unittest.TestCase):
     #Public
     
     def setUp(self):
-        pass
+        self.Constraint = self._make_mock_constraint_class()
+        self.constraint = self.Constraint('include', 'exclude') 
 
-    def test(self):
-        pass
+    def test___bool__(self):
+        self.assertTrue(self.constraint)
+        
+    def test___bool___with_empty_constraint(self):
+        constraint = self.Constraint()
+        self.assertFalse(constraint)
+        
+    def test___repr__(self):
+        self.assertIn('include', repr(self.constraint))
+        self.assertIn('exclude', repr(self.constraint))
+    
+    #Protected
+    
+    def _make_mock_constraint_class(self):
+        class MockConstraint(PatternConstraint):
+            #Protected
+            def _match(self, pattern, emitter):
+                pass
+        return MockConstraint
+                
