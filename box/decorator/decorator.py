01206970d154207c2c0e23447846a33f76ddfe0c
roll <roll@respect31.com>
2014-07-16 11:45:21 +0400

    improved decorator formatting

--- box/decorator/decorator.py
+++ box/decorator/decorator.py
@@ -3,26 +3,26 @@ from abc import ABCMeta, abstractmethod
 class DecoratorMetaclass(ABCMeta):
     """Metaclass to unify 1-2 step decorators implimentation.
     """
-    
-    #Public
-    
+
+    # Public
+
     def __call__(self, *args, **kwargs):
         decorator = object.__new__(self)
         if getattr(self, '__init__') is object.__init__:
-            #Init is not provided - 1 step
-            return decorator.__call__(*args, **kwargs)            
+            # Init is not provided - 1 step
+            return decorator.__call__(*args, **kwargs)
         else:
-            #Init is provided - 2 steps 
+            # Init is provided - 2 steps
             decorator.__init__(*args, **kwargs)
             return decorator
-    
+
     def __instancecheck__(self, instance):
         result = issubclass(instance, self)
         if not result:
             result = super().__instancecheck__(instance)
         return result
-        
-        
+
+
 class Decorator(metaclass=DecoratorMetaclass):
     """Base abstract class for unified 1-2 step decorators.
     
@@ -50,9 +50,9 @@ class Decorator(metaclass=DecoratorMetaclass):
       def function(self):
           pass              
     """
-    
-    #Public
-    
+
+    # Public
+
     @abstractmethod
     def __call__(self, function):
-        pass #pragma: no cover        
+        pass  # pragma: no cover
