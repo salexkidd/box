088719e544effc3cbede75faebe8917b22237db5
roll <roll@respect31.com>
2014-07-25 11:51:26 +0400

    simplified ObjnameConstraintTest

--- tests/component/findtools/test_objname.py
+++ tests/component/findtools/test_objname.py
@@ -3,7 +3,7 @@ import unittest
 from unittest.mock import Mock
 from box.findtools.objname import ObjnameConstraint
 
-class ObjnameConstraintTest_not_skip(unittest.TestCase):
+class ObjnameConstraintTest(unittest.TestCase):
 
     # Public
 
@@ -15,22 +15,17 @@ class ObjnameConstraintTest_not_skip(unittest.TestCase):
         constraint(self.emitter)
         self.assertFalse(self.emitter.skip.called)
 
-    def test___call___with_objname_is_regex(self):
-        mapper = ObjnameConstraint(re.compile('o.*'))
-        mapper(self.emitter)
-        self.assertFalse(self.emitter.skip.called)
-
-
-class ObjnameConstraintTest_skip(ObjnameConstraintTest_not_skip):
-
-    # Public
-
-    def test___call__(self):
+    def test___call___skip(self):
         constraint = ObjnameConstraint('x')
         constraint(self.emitter)
         self.assertTrue(self.emitter.skip.called)
 
     def test___call___with_objname_is_regex(self):
+        mapper = ObjnameConstraint(re.compile('o.*'))
+        mapper(self.emitter)
+        self.assertFalse(self.emitter.skip.called)
+
+    def test___call___skip_with_objname_is_regex(self):
         constraint = ObjnameConstraint(re.compile('x.*'))
         constraint(self.emitter)
         self.assertTrue(self.emitter.skip.called)
