ea621dbf92e71a24c7740cca2bdbce03e2e9fa1f
roll <roll@respect31.com>
2014-07-25 11:53:15 +0400

    simplified FilepathConstraintTest

--- tests/component/findtools/test_filepath.py
+++ tests/component/findtools/test_filepath.py
@@ -3,7 +3,7 @@ import unittest
 from unittest.mock import Mock
 from box.findtools.filepath import FilepathConstraint
 
-class FilepathConstraintTest_not_skip(unittest.TestCase):
+class FilepathConstraintTest(unittest.TestCase):
 
     # Public
 
@@ -15,22 +15,17 @@ class FilepathConstraintTest_not_skip(unittest.TestCase):
         constraint(self.emitter)
         self.assertFalse(self.emitter.skip.called)
 
-    def test___call___with_filepath_is_regex(self):
-        constraint = FilepathConstraint(re.compile('f.*'))
-        constraint(self.emitter)
-        self.assertFalse(self.emitter.skip.called)
-
-
-class FilepathConstraintTest_skip(FilepathConstraintTest_not_skip):
-
-    # Public
-
-    def test___call__(self):
+    def test___call___skip(self):
         constraint = FilepathConstraint('x*')
         constraint(self.emitter)
         self.assertTrue(self.emitter.skip.called)
 
     def test___call___with_filepath_is_regex(self):
+        constraint = FilepathConstraint(re.compile('f.*'))
+        constraint(self.emitter)
+        self.assertFalse(self.emitter.skip.called)
+
+    def test___call___with_filepath_is_regex_skip(self):
         constraint = FilepathConstraint(re.compile('x.*'))
         constraint(self.emitter)
         self.assertTrue(self.emitter.skip.called)
