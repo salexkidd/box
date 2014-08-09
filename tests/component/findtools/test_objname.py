6816b8745d50feba17516806db467ae708506094
roll <roll@respect31.com>
2014-08-09 11:28:15 +0400

    recovered test_objname

--- tests/component/findtools/test_objname.py
+++ tests/component/findtools/test_objname.py
@@ -9,24 +9,25 @@ class ObjnameConstraintTest(unittest.TestCase):
     # Public
 
     def setUp(self):
+        self.constraint = ObjnameConstraint()
         self.emitter = Mock(objname='objname')
 
     def test___call__(self):
-        constraint = ObjnameConstraint('objname')
-        constraint(self.emitter)
+        self.constraint.extend('objname', 'objname')
+        self.constraint(self.emitter)
         self.assertFalse(self.emitter.skip.called)
 
     def test___call___skip(self):
-        constraint = ObjnameConstraint('x')
-        constraint(self.emitter)
+        self.constraint.extend('objname', 'x')
+        self.constraint(self.emitter)
         self.assertTrue(self.emitter.skip.called)
 
     def test___call___with_objname_is_regex(self):
-        mapper = ObjnameConstraint(re.compile('o.*'))
-        mapper(self.emitter)
+        self.constraint.extend('objname', re.compile('o.*'))
+        self.constraint(self.emitter)
         self.assertFalse(self.emitter.skip.called)
 
     def test___call___with_objname_is_regex_skip(self):
-        constraint = ObjnameConstraint(re.compile('x.*'))
-        constraint(self.emitter)
+        self.constraint.extend('objname', re.compile('x.*'))
+        self.constraint(self.emitter)
         self.assertTrue(self.emitter.skip.called)
