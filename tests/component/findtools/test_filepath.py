da42371351a1676ed643de97bfd8d26c251b15c8
roll <roll@respect31.com>
2014-08-09 11:22:33 +0400

    recovered test_filepath

--- tests/component/findtools/test_filepath.py
+++ tests/component/findtools/test_filepath.py
@@ -9,24 +9,25 @@ class FilepathConstraintTest(unittest.TestCase):
     # Public
 
     def setUp(self):
+        self.constraint = FilepathConstraint()
         self.emitter = Mock(filepath='filepath')
 
     def test___call__(self):
-        constraint = FilepathConstraint('f*')
-        constraint(self.emitter)
+        self.constraint.extend('filepath', 'f*')
+        self.constraint(self.emitter)
         self.assertFalse(self.emitter.skip.called)
 
     def test___call___skip(self):
-        constraint = FilepathConstraint('x*')
-        constraint(self.emitter)
+        self.constraint.extend('filepath', 'x*')
+        self.constraint(self.emitter)
         self.assertTrue(self.emitter.skip.called)
 
     def test___call___with_filepath_is_regex(self):
-        constraint = FilepathConstraint(re.compile('f.*'))
-        constraint(self.emitter)
+        self.constraint.extend('filepath', re.compile('f.*'))
+        self.constraint(self.emitter)
         self.assertFalse(self.emitter.skip.called)
 
     def test___call___with_filepath_is_regex_skip(self):
-        constraint = FilepathConstraint(re.compile('x.*'))
-        constraint(self.emitter)
+        self.constraint.extend('filepath', re.compile('x.*'))
+        self.constraint(self.emitter)
         self.assertTrue(self.emitter.skip.called)
