713fbae756f4927bf1f0c07a47857564687b9368
roll <roll@respect31.com>
2014-08-09 11:29:58 +0400

    recovered test_objtype

--- tests/component/findtools/test_objtype.py
+++ tests/component/findtools/test_objtype.py
@@ -8,24 +8,25 @@ class ObjtypeConstraintTest(unittest.TestCase):
     # Public
 
     def setUp(self):
+        self.constraint = ObjtypeConstraint()
         self.emitter = Mock(object=ZeroDivisionError())
 
     def test___call__(self):
-        constraint = ObjtypeConstraint(ArithmeticError)
-        constraint(self.emitter)
+        self.constraint.extend('objtype', ArithmeticError)
+        self.constraint(self.emitter)
         self.assertFalse(self.emitter.skip.called)
 
     def test___call___skip(self):
-        constraint = ObjtypeConstraint(AssertionError)
-        constraint(self.emitter)
+        self.constraint.extend('objtype', AssertionError)
+        self.constraint(self.emitter)
         self.assertTrue(self.emitter.skip.called)
 
     def test___call___with_objtype_is_list(self):
-        constraint = ObjtypeConstraint([ArithmeticError, BufferError])
-        constraint(self.emitter)
+        self.constraint.extend('objtype', [ArithmeticError, BufferError])
+        self.constraint(self.emitter)
         self.assertFalse(self.emitter.skip.called)
 
     def test___call___with_objtype_is_list_skip(self):
-        constraint = ObjtypeConstraint([AssertionError, AttributeError])
-        constraint(self.emitter)
+        self.constraint.extend('objtype', [AssertionError, AttributeError])
+        self.constraint(self.emitter)
         self.assertTrue(self.emitter.skip.called)
