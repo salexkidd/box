ebb26e8e2bebb91b7cf188eac2e753af1b538438
roll <roll@respect31.com>
2014-08-09 11:25:32 +0400

    recovered test_maxdepth

--- tests/component/findtools/test_maxdeptch.py
+++ tests/component/findtools/test_maxdeptch.py
@@ -11,15 +11,16 @@ class MaxdepthConstraintTest(unittest.TestCase):
     def setUp(self):
         # Depth is 2
         self.emitter = Mock(filepath=os.path.join('file', 'path'))
+        self.constraint = MaxdepthConstraint()
 
     def test___call__(self):
-        constraint = MaxdepthConstraint(2)
-        constraint(self.emitter)
+        self.constraint.extend('maxdepth', 2)
+        self.constraint(self.emitter)
         self.assertFalse(self.emitter.skip.called)
         self.assertFalse(self.emitter.stop.called)
 
     def test___call___skip(self):
-        constraint = MaxdepthConstraint(1)
-        constraint(self.emitter)
+        self.constraint.extend('maxdepth', 1)
+        self.constraint(self.emitter)
         self.assertTrue(self.emitter.skip.called)
         self.assertTrue(self.emitter.stop.called)
