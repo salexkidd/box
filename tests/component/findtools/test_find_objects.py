9b95c8f7c91db24dff380ce588967a7f33a00424
roll <roll@respect31.com>
2014-08-09 11:58:12 +0400

    recovered test_find_objects

--- tests/component/findtools/test_find_objects.py
+++ tests/component/findtools/test_find_objects.py
@@ -1,7 +1,7 @@
 import re
 import unittest
 from functools import partial
-from unittest.mock import Mock, call, patch
+from unittest.mock import Mock, call
 from box.findtools.find_objects import find_objects
 
 
@@ -10,57 +10,41 @@ class find_objects_Test(unittest.TestCase):
     # Public
 
     def setUp(self):
-        self.find = self._make_mock_find()
-        self.pfind = partial(self.find, filepathes=['file1', 'file2'])
+        self.filepathes = ['file1', 'file2']
+        self.find = self._make_mock_find(self.filepathes)
+        self.pfind = partial(self.find, reducers=[list])
 
     def test(self):
-        objects = list(self.pfind(basedir='basedir'))
+        objects = self.pfind(basedir='basedir')
         self.assertTrue(objects)
-        self.find._Loader.assert_has_calls(
+        # Check SourceFileLoader call
+        self.find._SourceFileLoader.assert_has_calls(
             [call('basedir/file1', 'basedir/file1'),
              call('basedir/file2', 'basedir/file2')])
-        self.find._Loader.return_value.load_module.assert_has_calls(
+        # Check SourceFileLoader's return_value (loader) load_module call
+        self.find._SourceFileLoader.return_value.load_module.assert_has_calls(
             [call('basedir/file1'),
              call('basedir/file2')])
 
-    @patch.object(find_objects, '_find_files')
-    def test_without_files(self, find_files):
-        find_files.return_value = ['file1', 'file2']
-        objects = list(self.find(
-            basedir='basedir',
-            filename='filename',
-            notfilename='notfilename',
-            filepath='filepath',
-            notfilepath='notfilepath',
-            maxdepth='maxdepth'))
-        self.assertTrue(objects)
-        find_files.assert_called_with(
-            basedir='basedir',
-            filename='filename',
-            notfilename='notfilename',
-            filepath='filepath',
-            notfilepath='notfilepath',
-            maxdepth='maxdepth')
-
     def test_with_objname(self):
-        objects = list(self.pfind(objname='call'))
+        objects = self.pfind({'objname': 'call'})
         self.assertEqual(objects, [call, call])
 
     def test_with_objname_is_regex(self):
-        objects = list(self.pfind(objname=re.compile('^call')))
+        objects = self.pfind({'objname': re.compile('^call')})
         self.assertEqual(objects, [call, call])
 
     def test_with_objtype(self):
-        objects = list(self.pfind(objtype=type(call)))
+        objects = self.pfind({'objtype': type(call)})
         self.assertEqual(objects, [call, call])
 
     def test_with_objtype_is_list(self):
-        objects = list(self.pfind(objtype=[type(call)]))
+        objects = self.pfind({'objtype': [type(call)]})
         self.assertEqual(objects, [call, call])
 
     def test_with_mapper(self):
         mapper = lambda emitter: emitter.emit(emitter.objtype)
-        objects = list(self.pfind(objname='call', mappers=[mapper]))
+        objects = self.pfind({'objname': 'call'}, mappers=[mapper])
         self.assertEqual(objects, [type(call), type(call)])
 
     def test_with_reducer_and_fallback(self):
@@ -70,9 +54,10 @@ class find_objects_Test(unittest.TestCase):
 
     # Protected
 
-    def _make_mock_find(self):
+    def _make_mock_find(self, filepathes):
         class mock_find(find_objects):
             # Protected
-            _Loader = Mock(return_value=Mock(
+            _find_files = Mock(return_value=filepathes)
+            _SourceFileLoader = Mock(return_value=Mock(
                 load_module=Mock(return_value=unittest.mock)))
         return mock_find
