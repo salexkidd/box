ff8c7b09bd53365808feb7d630cc10425ddb42b2
roll <roll@respect31.com>
2014-08-09 10:54:52 +0400

    fixed test_filename

--- tests/component/findtools/test_filename.py
+++ tests/component/findtools/test_filename.py
@@ -9,24 +9,25 @@ class FilenameConstraintTest(unittest.TestCase):
     # Public
 
     def setUp(self):
+        self.constraint = FilenameConstraint()
         self.emitter = Mock(filename='filename')
 
     def test___call__(self):
-        constraint = FilenameConstraint('f*')
-        constraint(self.emitter)
+        self.constraint.extend('filename', 'f*')
+        self.constraint(self.emitter)
         self.assertFalse(self.emitter.skip.called)
 
     def test___call___skip(self):
-        constraint = FilenameConstraint('x*')
-        constraint(self.emitter)
+        self.constraint.extend('filename', 'x*')
+        self.constraint(self.emitter)
         self.assertTrue(self.emitter.skip.called)
 
     def test___call___with_filename_is_regex(self):
-        constraint = FilenameConstraint(re.compile('f.*'))
-        constraint(self.emitter)
+        self.constraint.extend('filename', re.compile('f.*'))
+        self.constraint(self.emitter)
         self.assertFalse(self.emitter.skip.called)
 
     def test___call___with_filename_is_regex_skip(self):
-        constraint = FilenameConstraint(re.compile('x.*'))
-        constraint(self.emitter)
+        self.constraint.extend('filename', re.compile('x.*'))
+        self.constraint(self.emitter)
         self.assertTrue(self.emitter.skip.called)
