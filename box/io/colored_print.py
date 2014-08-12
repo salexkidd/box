8a8d5940cfd3d4628156852dbd8df5d0361e1f3c
roll <roll@respect31.com>
2014-08-12 12:54:37 +0400

    improved ColoredPrint

--- box/io/colored_print.py
+++ box/io/colored_print.py
@@ -27,7 +27,6 @@ class ColoredPrint:
         'concealed': 8 ,
         'foreground': 30,
         'background': 40,
-        'bright': 30,
     }
 
     # Colors offsets
@@ -42,45 +41,65 @@ class ColoredPrint:
         'cyan': 6,
         'white': 7,
         'default': 9,
+        'bright_black': 60,
+        'bright_red': 61,
+        'bright_green': 62,
+        'bright_yellow': 63,
+        'bright_blue': 64,
+        'bright_magenta': 65,
+        'bright_cyan': 66,
+        'bright_white': 67,
     }
 
-    def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            attr = getattr(self, key, None)
-            if isinstance(attr, dict):
-                value = merge_dicts(attr, value)
-            setattr(self, key, value)
+    def __init__(self, **params):
+        for key in list(params):
+            if key in ['print', 'codes', 'offsets', 'color_offsets']:
+                value = params.pop(key, None)
+                if value is not None:
+                    if isinstance(value, dict):
+                        value = merge_dicts(getattr(self, key), value)
+                setattr(self, key, value)
+        self._params = params
+        self._buffer = None
         self._stack = []
 
     def __enter__(self):
-        # Set style
-        if self._stack:
-            code = self._stack[-1]
-            self.print(code)
+        if self._buffer is not None:
+            self._stack.append(self._buffer)
         return self
 
     def __exit__(self, cls, value, traceback):
-        # Reset style
-        code = self._make_code()
-        self.print(code)
-        # Recover style
         self._stack.pop()
-        for code in self._stack:
-            self.print(code)
 
-    def __call__(self, *args, **kwargs):
-        return self.print(*args, **kwargs)
+    def __call__(self, *values, **params):
+        # Decorate values
+        values = list(values)
+        if values:
+            # Apply style
+            for code in reversed(self._stack):
+                values[0] = code + str(values[0])
+            # Reset style
+            code = self._make_code()
+            values[-1] = str(values[-1]) + code
+        # Merge params
+        params = merge_dicts(self._params, params)
+        return self.print(*values, **params)
 
-    def style(self, **kwargs):
+    def style(self, **params):
         offsets = []
-        for key, value in kwargs.items():
-            if value is True:
-                offset = self.offsets[key]
-            elif value:
-                offset = self.offsets[key] + self.color_offsets[value]
+        for key, value in params.items():
+            try:
+                if key in ['foreground', 'background']:
+                    offset = self.offsets[key] + self.color_offsets[value]
+                elif value:
+                    offset = self.offsets[key]
+            except (KeyError, TypeError):
+                raise ValueError(
+                    'Bad value "{value}" for key "{key}"'.
+                    format(value=value, key=key))
             offsets.append(offset)
         code = self._make_code(offsets)
-        self._stack.append(code)
+        self._buffer = code
         return self
 
     # Protected
--- tests/component/io/test_colored_print.py
+++ tests/component/io/test_colored_print.py
@@ -16,14 +16,9 @@ class ColoredPrintTest(unittest.TestCase):
             self.cprint('test1')
             with self.cprint.style(background='green'):
                 self.cprint('test2')
-            self.cprint('test3')
+            self.cprint('test3', 'test4', kwarg1='kwarg1')
         # Check print call
         self.print.assert_has_calls([
-            call('\x1b[31;47m'),
-            call('test1'),
-            call('\x1b[42m'),
-            call('test2'),
-            call('\x1b[m'),
-            call('\x1b[31;47m'),
-            call('test3'),
-            call('\x1b[m')])
+            call('\x1b[31;47mtest1\x1b[m'),
+            call('\x1b[31;47m\x1b[42mtest2\x1b[m'),
+            call('\x1b[31;47mtest3', 'test4\x1b[m', kwarg1='kwarg1'), ])
