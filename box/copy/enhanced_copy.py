4e4899234d465dd35ed5b8b5a40127f542e5fcf6
roll <roll@respect31.com>
2014-08-23 10:36:28 +0400

    formatted enhanced_copy docs

--- box/copy/enhanced_copy.py
+++ box/copy/enhanced_copy.py
@@ -4,14 +4,24 @@ from copy import copy as python_copy
 def enhanced_copy(obj, *args, **kwargs):
     """Shallow copy operation on arbitrary Python objects.
 
-    :param mixed obj: object to copy
-    :param tuple args: args to pass to object's __copy__ method
-    :param dict kwargs: kwargs to pass to object's __copy__ method
-
     Difference with python library module is in __copy__ method priority:
 
       - if object has __copy__ method function calls it with args and kwarg
       - if object hasn't __copy__ method function acts like copy.copy
+
+    Parameters
+    ----------
+    obj: mixed
+        Object to copy.
+    args: tuple
+        Args to pass to object's __copy__ method.
+    kwargs: dict
+        Kwargs to pass to object's __copy__ method.
+
+    Returns
+    -------
+    mixed
+        Shallow copy of the object.
     """
     if hasattr(obj, '__copy__'):
         return obj.__copy__(*args, **kwargs)
