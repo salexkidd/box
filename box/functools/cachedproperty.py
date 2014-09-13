f0307a092a2641fb58d54ff092bb59945a02e968
roll <roll@respect31.com>
2014-09-13 11:46:18 +0400

    fixed functools protected/private

--- box/functools/cachedproperty.py
+++ box/functools/cachedproperty.py
@@ -5,69 +5,69 @@ class cachedproperty:
     attribute_name = '_box_functools_cached_properties'
 
     def __init__(self, fget=None, fset=None, fdel=None, doc=None):
-        self._fget = fget
-        self._fset = fset
-        self._fdel = fdel
-        self._doc = doc
+        self.__fget = fget
+        self.__fset = fset
+        self.__fdel = fdel
+        self.__doc = doc
 
     def __get__(self, obj, cls):
-        if self._fget:
-            cache = self._get_object_cache(obj)
-            if self._name not in cache:
-                cache[self._name] = self._fget(obj)
-            return cache[self._name]
+        if self.__fget:
+            cache = self.__get_object_cache(obj)
+            if self.__name not in cache:
+                cache[self.__name] = self.__fget(obj)
+            return cache[self.__name]
         else:
             raise AttributeError('Can\'t get attribute')
 
     def __set__(self, obj, value):
-        if self._fset:
-            self._fset(obj, value)
+        if self.__fset:
+            self.__fset(obj, value)
         else:
             raise AttributeError('Can\'t set attribute')
 
     def __delete__(self, obj):
-        if self._fdel:
-            self._fdel(obj)
+        if self.__fdel:
+            self.__fdel(obj)
         else:
             raise AttributeError('Can\'t delete attribute')
 
     @property
     def __doc__(self):
-        if self._doc:
-            return self._doc
-        elif self._fget:
-            return self._fget.__doc__
+        if self.__doc:
+            return self.__doc
+        elif self.__fget:
+            return self.__fget.__doc__
         else:
             return None
 
     def getter(self, fget):
-        self._fget = fget
+        self.__fget = fget
         return self
 
     def setter(self, fset):
-        self._fset = fset
+        self.__fset = fset
         return self
 
     def deleter(self, fdel):
-        self._fdel = fdel
+        self.__fdel = fdel
         return self
 
     @classmethod
     def set(cls, obj, name, value):
-        cache = cls._get_object_cache(obj)
+        cache = cls.__get_object_cache(obj)
         cache[name] = value
 
     @classmethod
     def reset(cls, obj, name):
-        cache = cls._get_object_cache(obj)
+        cache = cls.__get_object_cache(obj)
         cache.pop(name, None)
 
     # Protected
 
     @property
-    def _name(self):
-        return self._fget.__name__
+    def __name(self):
+        return self.__fget.__name__
 
     @classmethod
-    def _get_object_cache(cls, obj):
+    def __get_object_cache(cls, obj):
         return obj.__dict__.setdefault(cls.attribute_name, {})
