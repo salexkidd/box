a4596d71ae04434e633e26d4a3a5a8f469382fd2
roll <roll@respect31.com>
2014-09-23 13:30:06 +0400

    added package test to import_object

--- tests/component/importlib/test_object.py
+++ tests/component/importlib/test_object.py
@@ -8,21 +8,25 @@ class import_object_Test(unittest.TestCase):
 
     # Tests
 
-    def test_with_name(self):
+    def test(self):
         self.assertIs(
             component.import_object('unittest.mock.Mock'), Mock)
 
-    def test_with_name_and_module(self):
+    def test_with_module(self):
         self.assertIs(
             component.import_object('Mock', module='unittest.mock'), Mock)
+
+    def test_with_package(self):
+        self.assertIs(
+            component.import_object('.Mock', package='unittest.mock'), Mock)
 
     def test_with_name_in_bad_format(self):
         self.assertRaises(ValueError,
             component.import_object, 'unittest')
 
-    def test_with_name_for_no_object(self):
+    def test_with_name_is_not_existent(self):
         self.assertRaises(AttributeError,
-            component.import_object, 'unittest.mock.no_object')
+            component.import_object, 'unittest.mock.not_existent')
 
     def test_with_name_is_object(self):
         self.assertIs(component.import_object(Mock), Mock)
